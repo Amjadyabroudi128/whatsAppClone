27cfa4196a03d47da093ef18effd0b0e8f5da444
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-14 16:01:55 +0000

    privacy text color

--- lib/components/TextStyles.dart
+++ lib/components/TextStyles.dart
@@ -4,4 +4,5 @@ import 'package:flutter/material.dart';
 
 class Textstyles {
   static TextStyle welcome = TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
+  static TextStyle privacy = TextStyle(color: Colors.green);
  }
