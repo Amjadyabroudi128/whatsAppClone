1958e60d1c952f88c571d00096a53fcc6faf5714
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-14 16:03:12 +0000

    privacy text color

--- lib/components/TextStyles.dart
+++ lib/components/TextStyles.dart
@@ -1,8 +1,9 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/core/ColorHelper.dart';
 
 class Textstyles {
   static TextStyle welcome = TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
-  static TextStyle privacy = TextStyle(color: Colors.green);
+  static TextStyle privacy = TextStyle(color: ColorHelper.TC);
  }
