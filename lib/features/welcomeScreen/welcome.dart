b16109db54eb527b958a103ce734f4860fca57e8
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-15 19:06:07 +0000

    Resolved merge conflicts

--- lib/features/welcomeScreen/welcome.dart
+++ lib/features/welcomeScreen/welcome.dart
@@ -29,7 +29,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           SizedBox(height: 50,),
           welcomeText(),
           SizedBox(height: 18,),
-          Text("Hey"),
+          Text("Hello"),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 30),
             child: richText(),
