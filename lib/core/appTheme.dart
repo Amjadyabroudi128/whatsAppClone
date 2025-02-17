52e751b0de9161a2ecc1bc957a07cc3d1150c14a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-17 14:14:18 +0000

    outline border

--- lib/core/appTheme.dart
+++ lib/core/appTheme.dart
@@ -3,12 +3,16 @@
  import 'package:flutter/material.dart';
 
  class myTheme {
+   static OutlinedBorder circular = RoundedRectangleBorder(
+     borderRadius: BorderRadius.circular(10)
+   );
   static final ThemeData appTheme = ThemeData().copyWith(
        elevatedButtonTheme: ElevatedButtonThemeData(
          style: ElevatedButton.styleFrom(
              backgroundColor: Colors.green,
              foregroundColor: Colors.white,
-             textStyle: TextStyle(fontSize: 18)
+             textStyle: TextStyle(fontSize: 18),
+           shape: circular
          ),
        ),
    );
