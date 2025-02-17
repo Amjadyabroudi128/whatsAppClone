6cf8fd53f876d882617ca6dd6490213e0eecd50a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-17 17:55:41 +0000

    navigation

--- lib/features/welcomeScreen/Widgets/ElevatedButton.dart
+++ lib/features/welcomeScreen/Widgets/ElevatedButton.dart
@@ -8,7 +8,9 @@ class elevatedBtn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: (){},
+      onPressed: (){
+        Navigator.of(context).pushNamed("sign up");
+      },
       child: Text("Agree and Continue"),
     );
   }
