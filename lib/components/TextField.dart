226d1a1a7dcf9b9eb07cc63311de9e9a166fe6ec
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-18 16:38:46 +0000

    obsecure text

--- lib/components/TextField.dart
+++ lib/components/TextField.dart
@@ -4,15 +4,18 @@ class kTextField extends StatelessWidget {
   final Widget? label;
   final TextEditingController? myController;
   final Widget? myIcon;
-  const kTextField({super.key, this.label, this.myController, this.myIcon});
+  final bool obsecureText;
+  const kTextField({super.key, this.label, this.myController, this.myIcon, this.obsecureText = false});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: myController,
+      obscureText: obsecureText,
       decoration: InputDecoration(
         label: label,
         prefixIcon: myIcon,
+
       ),
     );
   }
