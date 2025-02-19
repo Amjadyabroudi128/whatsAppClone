3ea7b17480ee522e122e62919d2c8c49c1d657a9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-19 17:56:40 +0000

    signout

--- lib/features/testingScreen/testName.dart
+++ lib/features/testingScreen/testName.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/features/welcomeScreen/welcome.dart';
 
 class Testname extends StatefulWidget {
   final String? name;
@@ -12,6 +13,22 @@ class _TestnameState extends State<Testname> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => WelcomeScreen(),
+                ),
+              );
+            },
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
       body: Center(
         child: Text("Hello ${widget.name}"),
       ),
