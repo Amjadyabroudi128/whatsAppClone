799adf7e730a9132a511548307eb3ae70800007d
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-20 16:44:16 +0000

    text styles colors

--- lib/components/TextStyles.dart
+++ lib/components/TextStyles.dart
@@ -1,8 +1,9 @@
 
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/core/MyColors.dart';
 class Textstyles {
   static TextStyle welcome = TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
-  static TextStyle privacy = TextStyle(color: Colors.green);
-  static TextStyle read = TextStyle(fontSize: 20, color: Colors.grey);
-  static TextStyle forgotPass = TextStyle(color: Colors.blueGrey);
+  static TextStyle privacy = TextStyle(color: myColors.TC);
+  static TextStyle read = TextStyle(fontSize: 20, color: myColors.BG);
+  static TextStyle forgotPass = TextStyle(color: myColors.forgotPass);
  }
--- lib/core/MyColors.dart
+++ lib/core/MyColors.dart
@@ -6,6 +6,6 @@ class myColors {
    static Color BG = Colors.grey.shade300;
    static Color TC = Colors.green;
    static  Color FG = Colors.white;
-   static Color myName = Colors.orange;
    static Color lol = Colors.yellow;
+   static Color forgotPass = Colors.blueGrey;
  }
