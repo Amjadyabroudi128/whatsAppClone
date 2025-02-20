1da5e6c9fbe729b9f79b5fce2024c1949a0dad72
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-20 16:46:49 +0000

    text styles colors

--- lib/core/MyColors.dart
+++ lib/core/MyColors.dart
@@ -5,6 +5,7 @@
 class myColors {
    static Color BG = Colors.grey.shade300;
    static Color TC = Colors.green;
+   static Color familyText = Colors.grey;
    static  Color FG = Colors.white;
    static Color lol = Colors.yellow;
    static Color forgotPass = Colors.blueGrey;
