c95a5b273bc45c909b82dd6bdbb27a0452cd6934
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-21 18:15:59 +0000

    screen messages

--- lib/features/testingScreen/testName.dart
+++ lib/features/testingScreen/testName.dart
@@ -59,7 +59,7 @@ class _TestnameState extends State<Testname> {
                         child: Container(
                           padding: EdgeInsets.all(10.0),
                           decoration: BoxDecoration(
-                            color: messages[index].isme ? Colors.blue : Colors.grey[300],
+                            color: messages[index].isme ? Colors.green[400] : Colors.grey[300],
                             borderRadius: BorderRadius.circular(10.0),
                           ),
                           child: Text(messages[index].text),
