f52d9685f185390e2416b2cbbfa2e46ac2bdedd9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-21 18:14:34 +0000

    app theme

--- lib/core/appTheme.dart
+++ lib/core/appTheme.dart
@@ -4,6 +4,10 @@
 import 'package:whatsappclone/core/MyColors.dart';
 
  class myTheme {
+   static OutlineInputBorder fieldBorder = OutlineInputBorder(
+     borderRadius: BorderRadius.circular(8),
+     borderSide: BorderSide(width: 1,)
+   );
    static OutlinedBorder circular = RoundedRectangleBorder(
      borderRadius: BorderRadius.circular(10)
    );
@@ -16,5 +20,14 @@ import 'package:whatsappclone/core/MyColors.dart';
            shape: circular
          ),
        ),
+    inputDecorationTheme: InputDecorationTheme(
+      enabledBorder: fieldBorder,
+      // focusedBorder: fieldBorder,
+      // focusColor: Colors.black,
+      labelStyle: TextStyle(color: Colors.black, fontSize: 17),
+      hintStyle: TextStyle(fontSize: 19, color: Colors.black),
+      floatingLabelStyle: TextStyle(fontSize: 25, color: Colors.grey)
+    )
+
    );
  }
