1fc0d092cf4f5f4a692dbcb04add506a0ce52bab
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-25 19:41:48 +0000

    firebase auth

---
+++ lib/Firebase/FirebaseAuth.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+ class Firebase {
+   final FirebaseAuth auth = FirebaseAuth.instance;
+   Future<void> createEmailPassword(String email, String password) async {
+     auth.createUserWithEmailAndPassword(email: email, password: password);
+  }
+  Future<void> SigninUser(String email, String password) async {
+     auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+  Future<void> SignOut ()async {
+     auth.signOut();
+  }
+ }
