fc31d4b2c462adb856f63456ff874a2bf23a180f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-02-27 15:19:52 +0000

    app bar

--- lib/components/TextStyles.dart
+++ lib/components/TextStyles.dart
@@ -9,6 +9,6 @@ class Textstyles {
   static TextStyle hintStyle = TextStyle(color: Colors.grey, fontSize: 16);
   static TextStyle label = TextStyle(color: myColors.labelClr, fontSize: 15);
   static TextStyle floating = TextStyle(fontSize: 25, color: myColors.floating);
-  static TextStyle appBar = TextStyle(color: Colors.white, fontWeight: FontWeight.w500);
+  static TextStyle appBar = TextStyle(color: myColors.FG, fontWeight: FontWeight.w500);
   static TextStyle Ebtn =  TextStyle(fontSize: 18);
  }
