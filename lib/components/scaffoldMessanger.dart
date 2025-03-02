cba0088a6d42f4b6b462b31c8d51f765521937d2
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-02 15:22:28 +0000

    scaffold string

--- lib/components/scaffoldMessanger.dart
+++ lib/components/scaffoldMessanger.dart
@@ -1,10 +1,10 @@
 
  import 'package:flutter/material.dart';
 
-void showSnackbar (BuildContext context , Widget message, {Duration? duration}) {
+void showSnackbar (BuildContext context , String message, {Duration? duration}) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      content: message,
+      content: Text(message),
       duration: duration ?? Duration(seconds: 3),
     )
   );
