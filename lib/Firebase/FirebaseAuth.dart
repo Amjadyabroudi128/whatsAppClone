61a529ac15fd305d21522b6a6e959b699093f945
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-03 16:31:49 +0000

    flutter toast

--- lib/Firebase/FirebaseAuth.dart
+++ lib/Firebase/FirebaseAuth.dart
@@ -1,11 +1,31 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsappclone/components/scaffoldMessanger.dart';
+import 'package:fluttertoast/fluttertoast.dart';
  class FirebaseService {
    final FirebaseAuth auth = FirebaseAuth.instance;
    Future<void> createEmailPassword(String email, String password) async {
-     auth.createUserWithEmailAndPassword(email: email, password: password);
-  }
+     try {
+       await auth.createUserWithEmailAndPassword(email: email, password: password);
+
+     } on FirebaseAuthException catch (e) {
+       String message = '';
+       if (e.code == 'weak-password') {
+         message = 'The password provided is too weak.';
+       } else if (e.code == 'email-already-in-use') {
+         message = 'An account already exists with that email.';
+       }
+       Fluttertoast.showToast(
+         msg: message,
+         toastLength: Toast.LENGTH_LONG,
+         gravity: ToastGravity.SNACKBAR,
+         backgroundColor: Colors.black54,
+         textColor: Colors.white,
+         fontSize: 14.0,
+       );
+     }
+     catch (e){}
+
+   }
    Future<void> SigninUser(BuildContext context, String email, String password) async {
      await auth.signInWithEmailAndPassword(email: email, password: password);
    }
