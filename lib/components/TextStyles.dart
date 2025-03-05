293d310c35cfa1a53b76a19c1f5e8a5f570f6ab8
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-05 14:27:23 +0000

    haveaccount style

--- lib/components/TextStyles.dart
+++ lib/components/TextStyles.dart
@@ -5,7 +5,7 @@ class Textstyles {
   static TextStyle welcome = TextStyle(fontSize: 22, fontWeight: FontWeight.bold);
   static TextStyle privacy = TextStyle(color: myColors.TC);
   static TextStyle read = TextStyle(fontSize: 20, color: myColors.familyText);
-  static TextStyle forgotPass = TextStyle(color: myColors.forgotPass);
+  static TextStyle haveAccount = TextStyle(color: myColors.forgotPass);
   static TextStyle hintStyle = TextStyle(color: Colors.grey, fontSize: 16);
   static TextStyle label = TextStyle(color: myColors.labelClr, fontSize: 15);
   static TextStyle floating = TextStyle(fontSize: 25, color: myColors.floating);
