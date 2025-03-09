1c006beb718e6f955e8af40c20ddf6d859c40eee
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-09 15:58:08 +0000

    add firebase fire store

---
+++ lib/Firebase/FirestoreMessages.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> createUserInFirestore(User user) async {
+  await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+    'email': user.email,
+    'uid': user.uid,
+    'online': true,
+    'lastSeen': FieldValue.serverTimestamp(),
+  }, SetOptions(merge: true));
+}
