5a37ab86b486444349ff7e5155efb261acffb52a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-09 18:47:20 +0000

    do nothing if empty

--- lib/features/SignUp/Widgets/signupBtn.dart
+++ lib/features/SignUp/Widgets/signupBtn.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/Firebase/FirebaseAuth.dart';
 import 'package:whatsappclone/components/ElevatedBtn.dart';
@@ -8,21 +7,22 @@ class signUpBtn extends StatelessWidget {
   const signUpBtn({
     super.key,
     required this.myEmail,
-    required this.firebase,
     required this.pass,
   });
 
   final TextEditingController myEmail;
   final TextEditingController pass;
-  final FirebaseService firebase;
+
   @override
   Widget build(BuildContext context) {
+    FirebaseService firebase =  FirebaseService();
     return kElevatedBtn(
       onPressed: () async {
         if(myEmail.text.isEmpty || pass.text.isEmpty) {
           myToast("please fill all the fields");
-          firebase.createEmailPassword(context, myEmail.text, pass.text);
+          return;
         }
+          await firebase.createEmailPassword(context, myEmail.text, pass.text);
       },
       child: Text("Sign Up"),
     );
