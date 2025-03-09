9596427a87a9b5540c881294afb7affd0ae6050f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-09 16:10:04 +0000

    main screen

--- lib/features/mainChats/mainChat.dart
+++ lib/features/mainChats/mainChat.dart
@@ -1,32 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/components/TextStyles.dart';
 import 'package:whatsappclone/components/icons.dart';
 import 'package:whatsappclone/core/MyColors.dart';
 
-class Mainchat extends StatefulWidget {
+import '../privateChatScreen/chatScreen.dart';
+
+class Mainchat extends StatelessWidget {
   final String? name;
   const Mainchat({super.key, this.name});
 
   @override
-  State<Mainchat> createState() => _MainchatState();
-}
-
-class _MainchatState extends State<Mainchat> {
-  @override
   Widget build(BuildContext context) {
+    final User currentUser = FirebaseAuth.instance.currentUser!;
+    final usersdb = FirebaseFirestore.instance.collection("users");
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Chats ${widget.name}", style: Textstyles.appBar,),
+        title: Text("Chats", style: Textstyles.appBar,),
         backgroundColor: myColors.TC,
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: Text("Hello"),
+      body: StreamBuilder(
+        stream: usersdb.snapshots(),
+        builder: (context, snapshot) {
+          if(!snapshot.hasData)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          var users = snapshot.data!.docs.where((doc) => doc['uid'] != currentUser.uid).toList();
+          if (users.isEmpty) return Center(child: Text("No other users found"));
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              var userData = users[index];
+              bool isOnline = userData['online'] ?? false;
+              return ListTile(
+                leading: Icon(Icons.account_circle, size: 35,),
+                title: Text(userData["email"]),
+                onTap: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => PrivateChatScreen(
+                        receiverId: userData['uid'],
+                        receiverEmail: userData['email'],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: icons.add,
-      )
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: (){},
+      //   child: icons.add,
+      // )
     );
   }
 }
