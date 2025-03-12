70cc8e7779e49aed5b8a722ea9e52ff6f5a54f18
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-12 13:17:48 +0000

    contacts screen fix

--- lib/features/contacts/contacts.dart
+++ lib/features/contacts/contacts.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsappclone/components/TextStyles.dart';
+import 'package:whatsappclone/core/MyColors.dart';
 
+import '../testingScreen/Widgets/signoutBtn.dart';
 class Contacts extends StatefulWidget {
   const Contacts({super.key});
 
@@ -10,6 +16,24 @@ class Contacts extends StatefulWidget {
 class _ContactsState extends State<Contacts> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(top: 17),
+              child: signoutBtn(),
+            ),
+          ],
+          title: Padding(
+            padding: const EdgeInsets.only(top: 18),
+            child: Text("Chats", style: Textstyles.appBar,),
+          ),
+          backgroundColor: myColors.TC,
+          automaticallyImplyLeading: false,
+        ),
+        body: Center(
+          child: Text("SALAM"),
+        ),
+    );
   }
 }
