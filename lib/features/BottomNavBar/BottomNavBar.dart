9b55ffb91e14df368bdd7c6411d407e0b1601c61
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-12 12:37:26 +0000

    bottom nav bar

---
+++ lib/features/BottomNavBar/BottomNavBar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Bottomnavbar extends StatefulWidget {
+  const Bottomnavbar({super.key});
+
+  @override
+  State<Bottomnavbar> createState() => _BottomnavbarState();
+}
+
+class _BottomnavbarState extends State<Bottomnavbar> {
+  int _selectedIndex = 0;
+  final _widgetOptions = [
+    Text("Contacts "),
+    Text("chats"),
+    Text('Search Page'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.contacts),
+              label: 'Contacts',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Chats',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.blue[800],
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
