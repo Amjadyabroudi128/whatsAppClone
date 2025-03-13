ec387ed7287d0dc83b64525ddb13df1dcbe32d97
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-13 15:09:41 +0000

    removed comments

--- lib/features/mainChats/mainScreen.dart
+++ lib/features/mainChats/mainScreen.dart
@@ -1,9 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsappclone/components/TextStyles.dart';
-import 'package:whatsappclone/components/icons.dart';
-import 'package:whatsappclone/core/MyColors.dart';
 
 import '../BottomNavBar/BottomNavBar.dart';
 import '../testingScreen/Widgets/signoutBtn.dart';
