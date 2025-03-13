2750c071591951087b795210b61c6d96837534c8
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-13 13:29:37 +0000

    padding

--- lib/features/contacts/contacts.dart
+++ lib/features/contacts/contacts.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:whatsappclone/components/TextStyles.dart';
+import 'package:whatsappclone/components/padding.dart';
 import 'package:whatsappclone/core/MyColors.dart';
 
 import '../testingScreen/Widgets/signoutBtn.dart';
@@ -14,17 +14,19 @@ class Contacts extends StatefulWidget {
 }
 
 class _ContactsState extends State<Contacts> {
+  User? user = FirebaseAuth.instance.currentUser;
+  final FirebaseAuth auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           actions: [
-            Padding(
+            myPadding(
               padding: const EdgeInsets.only(top: 17),
               child: signoutBtn(),
             ),
           ],
-          title: Padding(
+          title: myPadding(
             padding: const EdgeInsets.only(top: 18),
             child: Text("Chats", style: Textstyles.appBar,),
           ),
