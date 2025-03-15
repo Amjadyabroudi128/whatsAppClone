967cf5197c8b8c54bcc9cbc400b2fe18444ad9ea
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-15 21:44:39 +0000

    trying delete account

--- lib/features/mainChats/mainScreen.dart
+++ lib/features/mainChats/mainScreen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/Firebase/FirebaseAuth.dart';
 
 import '../BottomNavBar/BottomNavBar.dart';
 import '../testingScreen/Widgets/signoutBtn.dart';
