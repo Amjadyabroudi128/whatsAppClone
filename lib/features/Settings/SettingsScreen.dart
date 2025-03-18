f54a6609f7b98817c20312e178fa3e17476e22d6
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-18 15:22:35 +0000

    delete account

--- lib/features/Settings/SettingsScreen.dart
+++ lib/features/Settings/SettingsScreen.dart
@@ -62,13 +62,7 @@ class _SettingScreenState extends State<SettingScreen> {
                         endIndent: 10,
                         color: Colors.grey.shade400,
                       ),
-                      ListTile(
-                          title: Text("Delete Account", style: TextStyle(fontSize: 19, color: Colors.red),),
-                          trailing: kIconButton(
-                            onPressed: (){} ,
-                            myIcon: Icon(Icons.remove)
-                          )
-                      ),
+                      deleteAccount(),
                     ],
                   ),
                 ),
@@ -77,6 +71,23 @@ class _SettingScreenState extends State<SettingScreen> {
           );
         },
       ),
+    );
+  }
+}
+
+class deleteAccount extends StatelessWidget {
+  const deleteAccount({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        title: Text("Delete Account", style: TextStyle(fontSize: 19, color: Colors.red),),
+        trailing: kIconButton(
+          onPressed: (){} ,
+          myIcon: Icon(Icons.remove)
+        )
     );
   }
 }
