a0fb019eaf756677e81739d2a8b5af3507eba188
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-18 15:26:18 +0000

    delete account color

--- lib/features/Settings/Widget/deleteAccount.dart
+++ lib/features/Settings/Widget/deleteAccount.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/core/MyColors.dart';
 
 import '../../../components/iconButton.dart';
 
@@ -10,7 +11,7 @@ class deleteAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text("Delete Account", style: TextStyle(fontSize: 19, color: Colors.red),),
+        title: Text("Delete Account", style: TextStyle(fontSize: 19, color: myColors.delete),),
         trailing: kIconButton(
             onPressed: (){} ,
             myIcon: Icon(Icons.remove)
