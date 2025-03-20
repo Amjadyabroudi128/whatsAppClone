6cd8ca1fc9149b95cdbb29726356a9a2e507fd13
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-20 15:03:20 +0000

    dark and normal theme

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyAppState extends State<MyApp> {
       title: 'Flutter Demo',
       themeMode: ThemeMode.system, // theme depends on system
       theme: myTheme.appTheme,
-      darkTheme: myTheme.appTheme,
+      darkTheme: myTheme.darkTheme,
       // initialRoute: "nameScreen",
       routes: {
         "sign up": (context) => const Signupscreen(),
