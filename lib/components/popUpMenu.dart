8d2b225ce3a65aa4faa9ca6fe0010d6467f18f63
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-24 15:11:15 +0000

    .

--- lib/components/popUpMenu.dart
+++ lib/components/popUpMenu.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
-class MyPopUpMenu extends StatelessWidget {
-  final PopupMenuItemBuilder itemBuilder;
-  final PopupMenuItemSelected onSelected;
+class MyPopUpMenu<T> extends StatelessWidget {
+  final PopupMenuItemBuilder<T> itemBuilder;
+  final PopupMenuItemSelected<T> onSelected;
+  final AnimationStyle? popUpAnimationStyle;
   final Widget? icon;
   final Color? iconColor;
-  const MyPopUpMenu({
-    super.key,
-    required this.itemBuilder,
-    required this.onSelected,
-    this.icon,
-    this.iconColor,
-  });
+  const MyPopUpMenu({super.key, required this.itemBuilder, required this.onSelected, this.popUpAnimationStyle, this.icon, this.iconColor, });
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton(
-      icon: icon ?? Icon(Icons.more_vert, color: iconColor),
+    const kDuration = Duration(milliseconds: 400);
+    return PopupMenuButton<T>(
+      iconColor: iconColor,
+      icon: icon,
       itemBuilder: itemBuilder,
       onSelected: onSelected,
+      popUpAnimationStyle: AnimationStyle(
+          duration: kDuration
+      ),
     );
   }
 }
