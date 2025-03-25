e7bfa2e8b809121c5269852eb24ce11506185499
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-25 00:12:44 +0000

    pick color for chat

--- lib/features/Settings/Widget/themeCard.dart
+++ lib/features/Settings/Widget/themeCard.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/components/SizedBox.dart';
+import 'package:whatsappclone/features/Settings/Widget/dividerWidget.dart';
 
+import '../../../components/iconButton.dart';
 import '../../../components/popUpMenu.dart';
 import '../../../core/appTheme.dart';
 import '../../../core/icons.dart';
@@ -19,38 +22,55 @@ class themeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-          title: Text("Theme"),
-          trailing: MyPopUpMenu(
-              icon: icons.arrowForward,
-              itemBuilder: (context) {
-                return [
-                  PopupMenuItem<myPop>(
-                    value: myPop.off,
-                    child: Text(("Off")),
-                  ),
-                  PopupMenuItem<myPop>(
-                    value: myPop.darkTheme ,
-                    child: Text(("Dark Theme")),
-                  ),
-                  PopupMenuItem<myPop>(
-                    value: myPop.system,
-                    child: Text(("System theme")),
-                  ),
-                ];
-              },
-              onSelected: (value) {
-                // if (!mounted) return; // Prevent operations if widget is not active
+      child: Column(
+        children: [
+          ListTile(
+              title: Text("Theme"),
+              trailing: MyPopUpMenu(
+                  icon: icons.arrowForward,
+                  itemBuilder: (context) {
+                    return [
+                      PopupMenuItem<myPop>(
+                        value: myPop.off,
+                        child: Text(("Off")),
+                      ),
+                      PopupMenuItem<myPop>(
+                        value: myPop.darkTheme ,
+                        child: Text(("Dark Theme")),
+                      ),
+                      PopupMenuItem<myPop>(
+                        value: myPop.system,
+                        child: Text(("System theme")),
+                      ),
+                    ];
+                  },
+                  onSelected: (value) {
+                    // if (!mounted) return; // Prevent operations if widget is not active
 
-                if (value == myPop.off) {
-                  widget.onThemeChange?.call(myTheme.appTheme);
-                } else if (value == myPop.darkTheme) {
-                  widget.onThemeChange?.call(myTheme.darkTheme);
-                } else {
-                  widget.onThemeChange?.call(ThemeData());
-                }
-              }
+                    if (value == myPop.off) {
+                      widget.onThemeChange?.call(myTheme.appTheme);
+                    } else if (value == myPop.darkTheme) {
+                      widget.onThemeChange?.call(myTheme.darkTheme);
+                    } else {
+                      widget.onThemeChange?.call(ThemeData());
+                    }
+                  }
+              )
+          ),
+          divider(),
+          GestureDetector(
+            onTap: (){
+              Navigator.of(context).pushNamed("pickColor");
+            },
+            child: ListTile(
+              title: Text("Chat Theme"),
+                trailing: kIconButton(
+                    onPressed: (){} ,
+                    myIcon: Icon(Icons.color_lens_outlined)
+                )
+            ),
           )
+        ],
       ),
     );
   }
