ff70247bc01e502ed3c353371539b46ca5e20763
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-25 15:41:16 +0000

    global value listener

--- lib/features/chatScreen/chatScreen.dart
+++ lib/features/chatScreen/chatScreen.dart
@@ -1,20 +1,18 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsappclone/colorList/colorsList.dart';
-import 'package:whatsappclone/components/SizedBox.dart';
 import 'package:whatsappclone/components/iconButton.dart';
-import 'package:whatsappclone/components/popUpMenu.dart';
 import 'package:whatsappclone/core/icons.dart';
 import 'package:whatsappclone/core/MyColors.dart';
 import 'package:whatsappclone/core/appTheme.dart';
 import '../../components/TextField.dart';
-import '../../enums/enums.dart';
+import '../../globalState.dart';
 import '../../messageClass/messageClass.dart';
 
 class Testname extends StatefulWidget {
-  final String username;
-  const Testname({super.key, required this.username}); // Fixed the syntax error
+  final String? username;
+
+  const Testname({super.key, this.username});
 
   @override
   State<Testname> createState() => _TestnameState();
@@ -34,104 +32,81 @@ class _TestnameState extends State<Testname> {
       });
     }
   }
-  Color scaffold = Colors.white;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: scaffold,
-      appBar: AppBar(
-        backgroundColor: scaffold,
-        automaticallyImplyLeading: true,
-        title: Text("${widget.username}", style: TextStyle(fontSize: 16),), // Display user email
-        actions: [
-          MyPopUpMenu<Color>(
-            icon: Icon(Icons.color_lens),
-            itemBuilder: (context) {
-              return colorNames.keys.map((Color color) {
-                return PopupMenuItem<Color>(
-                  value: color,
-                  child: Row(
-                    children: <Widget>[
-                      CircleAvatar(
-                        radius: 10,
-                        backgroundColor: color,
-                      ),
-                      BoxSpacing(mWidth: 10,),
-                      Text(colorNames[color]!),
-                    ],
-                  ),
-                );
-              }).toList();
-            },
-            onSelected: (Color color) {
-              setState(() {
-                scaffold = color;
-              });
-            },
-          )
-
-        ],
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: messages.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Align(
-                        child: Text(messages[index].isme
-                            ? "${widget.username ?? 'Unknown'}" // Show user email
-                            : "User B"
-                        ),
-                        alignment: messages[index].isme
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
-                      ),
-                      Align(
-                        alignment: messages[index].isme
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
-                        child: Container(
-                          padding: EdgeInsets.all(10.0),
-                          decoration: BoxDecoration(
-                            color: messages[index].isme
-                                ? myColors.myMessage
-                                : myColors.message,
-                            borderRadius: myTheme.CircularContainer,
+    return ValueListenableBuilder<Color>(
+      valueListenable: selectedThemeColor, // Listen to theme changes
+      builder: (context, color, child) {
+        return Scaffold(
+          backgroundColor: color, // Use the selected color
+          appBar: AppBar(
+            backgroundColor: color,
+            automaticallyImplyLeading: true,
+            title: Text("${widget.username}", style: const TextStyle(fontSize: 16)),
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Align(
+                            child: Text(messages[index].isme
+                                ? "${widget.username ?? 'Unknown'}" // Show user email
+                                : "User B"),
+                            alignment: messages[index].isme
+                                ? Alignment.centerRight
+                                : Alignment.centerLeft,
                           ),
-                          child: Text(messages[index].text),
-                        ),
+                          Align(
+                            alignment: messages[index].isme
+                                ? Alignment.centerRight
+                                : Alignment.centerLeft,
+                            child: Container(
+                              padding: const EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                color: messages[index].isme
+                                    ? myColors.myMessage
+                                    : myColors.message,
+                                borderRadius: myTheme.CircularContainer,
+                              ),
+                              child: Text(messages[index].text),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(9.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: kTextField(
-                    myController: messageController,
-                    hint: "Add a message",
-                  ),
+                    );
+                  },
                 ),
-                kIconButton(onPressed: () {}, myIcon: icons.image, iconSize: 26),
-                kIconButton(
-                  onPressed: sendMessage,
-                  myIcon: icons.send,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(9.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: kTextField(
+                        myController: messageController,
+                        hint: "Add a message",
+                      ),
+                    ),
+                    kIconButton(onPressed: () {}, myIcon: icons.image, iconSize: 26),
+                    kIconButton(
+                      onPressed: sendMessage,
+                      myIcon: icons.send,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
+
