ed8a1809efec0d87f547391a204f3c7ffb521159
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-25 15:54:14 +0000

    refactored

--- lib/colorPicker/ColorPicking.dart
+++ lib/colorPicker/ColorPicking.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../colorList/colorsList.dart';
+import 'colorsList.dart';
 
 class Colorpicking extends StatefulWidget {
   const Colorpicking({super.key});
