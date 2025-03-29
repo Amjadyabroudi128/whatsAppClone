aa0c9a0b7562bc488378ee67f888a60e8584746c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-29 16:10:14 +0000

    sending messages

--- lib/features/chatScreen/chatScreen.dart
+++ lib/features/chatScreen/chatScreen.dart
@@ -1,6 +1,7 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/Firebase/FirebaseAuth.dart';
 import 'package:whatsappclone/components/iconButton.dart';
 import 'package:whatsappclone/core/icons.dart';
 import 'package:whatsappclone/core/MyColors.dart';
@@ -10,9 +11,10 @@ import '../../globalState.dart';
 import '../../messageClass/messageClass.dart';
 
 class Testname extends StatefulWidget {
-  final String? username;
+  final receiverName;
+  final  receiverId;
 
-  const Testname({super.key, this.username});
+  const Testname({super.key, this.receiverName, this.receiverId});
 
   @override
   State<Testname> createState() => _TestnameState();
@@ -20,72 +22,72 @@ class Testname extends StatefulWidget {
 
 class _TestnameState extends State<Testname> {
   final TextEditingController messageController = TextEditingController();
-
-  // Get current user
+  final FirebaseService service = FirebaseService();
   User? user = FirebaseAuth.instance.currentUser;
 
-  void sendMessage() {
+  void sendMessage() async {
     if (messageController.text.isNotEmpty) {
-      setState(() {
-        messages.add(Messages(text: messageController.text, isme: true));
-        messageController.clear();
-      });
+      await service.sendMessage(widget.receiverId, widget.receiverName, messageController.text);
+      messageController.clear();
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<Color>(
-      valueListenable: selectedThemeColor, // Listen to theme changes
+      valueListenable: selectedThemeColor,
       builder: (context, color, child) {
         return Scaffold(
-          backgroundColor: color, // Use the selected color
+          backgroundColor: color,
           appBar: AppBar(
             backgroundColor: color,
-            automaticallyImplyLeading: true,
-            title: Text("${widget.username}", style: const TextStyle(fontSize: 16)),
+            title: Text(widget.receiverName, style: const TextStyle(fontSize: 16)),
           ),
           body: Column(
             children: [
               Expanded(
-                child: ListView.builder(
-                  itemCount: messages.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Align(
-                            child: Text(messages[index].isme
-                                ? "${widget.username ?? 'Unknown'}" // Show user email
-                                : "User B"),
-                            alignment: messages[index].isme
-                                ? Alignment.centerRight
-                                : Alignment.centerLeft,
-                          ),
-                          Align(
-                            alignment: messages[index].isme
-                                ? Alignment.centerRight
-                                : Alignment.centerLeft,
-                            child: Container(
-                              padding: const EdgeInsets.all(10.0),
-                              decoration: BoxDecoration(
-                                color: messages[index].isme
-                                    ? myColors.myMessage
-                                    : myColors.message,
-                                borderRadius: myTheme.CircularContainer,
-                              ),
-                              child: Text(messages[index].text),
+                child: StreamBuilder(
+                  stream: service.getMessages(user!.uid, widget.receiverId),
+                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      return const Center(child: Text("No messages yet"));
+                    }
+                    var messages = snapshot.data!.docs.map((doc) => Messages(
+                      text: doc["message"],
+                      senderId: doc["senderId"],
+                      receiverId: doc["receiverId"],
+                      senderEmail: doc["senderEmail"],
+                      receiverEmail: doc["receiverEmail"],
+                    )).toList();
+
+                    return ListView.builder(
+                      itemCount: messages.length,
+                      itemBuilder: (context, index) {
+                        final msg = messages[index];
+                        bool isMe = msg.senderId == user!.uid;
+                        return Align(
+                          alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: isMe ? myColors.myMessage : myColors.message,
+                              borderRadius: myTheme.CircularContainer,
                             ),
+                            child: Text(msg.text),
                           ),
-                        ],
-                      ),
+                        );
+                      },
                     );
                   },
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.all(9.0),
+                padding: const EdgeInsets.all(8.0),
                 child: Row(
                   children: [
                     Expanded(
@@ -109,4 +111,3 @@ class _TestnameState extends State<Testname> {
     );
   }
 }
-
