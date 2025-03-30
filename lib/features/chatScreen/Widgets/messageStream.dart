eb8cf677d6e53defe958062d58b6bd4738f8a6ab
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-30 21:17:29 +0100

    stream builder

---
+++ lib/features/chatScreen/Widgets/messageStream.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../../Firebase/FirebaseAuth.dart';
+import '../../../messageClass/messageClass.dart';
+import '../chatScreen.dart';
+
+class MessageStream extends StatelessWidget {
+  const MessageStream({
+    super.key,
+    required this.service,
+    required this.user,
+    required this.widget,
+  });
+
+  final FirebaseService service;
+  final User? user;
+  final Testname widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: service.getMessages(user!.uid, widget.receiverId),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text("No messages yet"));
+        }
+        var messages = snapshot.data!.docs.map((doc) => Messages(
+          text: doc["message"],
+          senderId: doc["senderId"],
+          receiverId: doc["receiverId"],
+          senderEmail: doc["senderEmail"],
+          receiverEmail: doc["receiverEmail"],
+        )).toList();
+
+        return messagesAlign(messages: messages, user: user);
+      },
+    );
+  }
+}
--- lib/features/chatScreen/chatScreen.dart
+++ lib/features/chatScreen/chatScreen.dart
@@ -9,6 +9,7 @@ import 'package:whatsappclone/core/appTheme.dart';
 import '../../components/TextField.dart';
 import '../../globalState.dart';
 import '../../messageClass/messageClass.dart';
+import 'Widgets/messageStream.dart';
 
 class Testname extends StatefulWidget {
   final receiverName;
@@ -47,44 +48,7 @@ class _TestnameState extends State<Testname> {
           body: Column(
             children: [
               Expanded(
-                child: StreamBuilder(
-                  stream: service.getMessages(user!.uid, widget.receiverId),
-                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                      return const Center(child: Text("No messages yet"));
-                    }
-                    var messages = snapshot.data!.docs.map((doc) => Messages(
-                      text: doc["message"],
-                      senderId: doc["senderId"],
-                      receiverId: doc["receiverId"],
-                      senderEmail: doc["senderEmail"],
-                      receiverEmail: doc["receiverEmail"],
-                    )).toList();
-
-                    return ListView.builder(
-                      itemCount: messages.length,
-                      itemBuilder: (context, index) {
-                        final msg = messages[index];
-                        bool isMe = msg.senderId == user!.uid;
-                        return Align(
-                          alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              color: isMe ? myColors.myMessage : myColors.message,
-                              borderRadius: myTheme.CircularContainer,
-                            ),
-                            child: Text(msg.text),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
+                child: MessageStream(service: service, user: user, widget: widget),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -105,6 +69,41 @@ class _TestnameState extends State<Testname> {
                 ),
               ),
             ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+
+class messagesAlign extends StatelessWidget {
+  const messagesAlign({
+    super.key,
+    required this.messages,
+    required this.user,
+  });
+
+  final List<Messages> messages;
+  final User? user;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        final msg = messages[index];
+        bool isMe = msg.senderId == user!.uid;
+        return Align(
+          alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: isMe ? myColors.myMessage : myColors.message,
+              borderRadius: myTheme.CircularContainer,
+            ),
+            child: Text(msg.text),
           ),
         );
       },
