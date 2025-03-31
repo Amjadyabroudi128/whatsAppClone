b36ebba4a96d04f7bb7f1882a3c81e184392ba86
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-31 16:28:13 +0100

    adding colors

--- lib/colorPicker/colorsList.dart
+++ lib/colorPicker/colorsList.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 final Map<Color, String> colorNames = {
   Colors.red: "Red",
   Colors.green: "Green",
+  Colors.green.shade700: "shade700",
   Colors.blue: "Blue",
   Colors.orange: "Orange",
   Colors.purple: "Purple",
   Colors.yellow: "Yellow",
   Colors.pink: "Pink",
   Colors.teal: "Teal",
+  Colors.teal.shade800: "teal800",
   Colors.white: "white",
 };
 Color getTextColor(Color backgroundColor) {
