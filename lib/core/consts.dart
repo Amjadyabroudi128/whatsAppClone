231f3957b948ef4ffa07ce4b21694910126f5069
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-03-31 16:16:05 +0100

    container decoration

--- lib/core/consts.dart
+++ lib/core/consts.dart
@@ -2,5 +2,13 @@
 
 import 'package:flutter/material.dart';
 
+import 'appTheme.dart';
+
 EdgeInsetsGeometry containerPadding = EdgeInsets.all(10);
-EdgeInsetsGeometry containermargin = EdgeInsets.symmetric(vertical: 5, horizontal: 10),;
+EdgeInsetsGeometry containermargin = EdgeInsets.symmetric(vertical: 5, horizontal: 10);
+Decoration containerDecoration({Color? color, BorderRadiusGeometry? borderRadius}) {
+  return BoxDecoration(
+    color: color,
+    borderRadius: myTheme.CircularContainer,
+  );
+}
