fabef2d1cee4362612f6b68830523ec7be12359c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-04-01 14:54:56 +0100

    timestamp

--- lib/messageClass/messageClass.dart
+++ lib/messageClass/messageClass.dart
@@ -1,18 +1,22 @@
 
- class Messages {
+ import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages {
   final String text;
   final String? receiverId;
   final String? senderEmail;
   final String? senderId;
   final String? receiverEmail;
-  Messages({required this.text,  this.receiverEmail,  this.senderId,  this.receiverId, this.senderEmail});
+  final Timestamp? time;
+  Messages({required this.text,  this.receiverEmail,  this.senderId,  this.receiverId, this.senderEmail, this.time,});
   Map<String, dynamic> toMap (){
     return {
       "senderId" : senderId,
       "message": text,
       "receiverId": receiverId,
       "receiverEmail": receiverEmail,
-      "senderEmail": senderEmail
+      "senderEmail": senderEmail,
+      "time": time
     };
   }
  }
