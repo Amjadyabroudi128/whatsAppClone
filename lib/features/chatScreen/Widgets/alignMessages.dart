1ee9acb47b355502e2fd2aa06878e79308c04c40
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-04-01 14:54:23 +0100

    dateformat

--- lib/features/chatScreen/Widgets/alignMessages.dart
+++ lib/features/chatScreen/Widgets/alignMessages.dart
@@ -5,7 +5,7 @@ import 'package:whatsappclone/core/consts.dart';
 import '../../../core/MyColors.dart';
 import '../../../core/appTheme.dart';
 import '../../../messageClass/messageClass.dart';
-
+import 'package:intl/intl.dart';
 class messagesAlign extends StatelessWidget {
   const messagesAlign({
     super.key,
@@ -23,17 +23,35 @@ class messagesAlign extends StatelessWidget {
       itemBuilder: (context, index) {
         final msg = messages[index];
         bool isMe = msg.senderId == user!.uid;
-        return Align(
-          alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
-          child: Container(
-            margin:  containermargin,
-            padding:  containerPadding,
-            decoration: containerDecoration(
-              color: isMe ? myColors.myMessage : myColors.message,
-              borderRadius: myTheme.CircularContainer,
+        String formattedTime =
+             DateFormat.Hm().format(msg.time!.toDate());
+        String day = DateFormat.yMd().format(msg.time!.toDate());
+        return Column(
+          children: [
+            Text(day),
+            Align(
+              alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+              child: Container(
+                margin:  containermargin,
+                padding:  containerPadding,
+                decoration: containerDecoration(
+                  color: isMe ? myColors.myMessage : myColors.message,
+                  borderRadius: myTheme.CircularContainer,
+                ),
+                child: Column(
+                  children: [
+                    Text(msg.text),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 40,
+                      ),
+                      child: Text(formattedTime),
+                    )
+                  ],
+                ),
+              ),
             ),
-            child: Text(msg.text),
-          ),
+          ],
         );
       },
     );
