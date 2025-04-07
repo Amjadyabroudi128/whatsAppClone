b75fc0afe5fce6008ab1cdfae5252961441839f4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-04-07 15:08:09 +0100

    delete message

--- lib/features/chatScreen/Widgets/deleteMessage.dart
+++ lib/features/chatScreen/Widgets/deleteMessage.dart
@@ -1,13 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/Firebase/FirebaseAuth.dart';
+import 'package:whatsappclone/features/chatScreen/chatScreen.dart';
+import 'package:whatsappclone/messageClass/messageClass.dart';
 
 import '../../../components/TextStyles.dart';
 import '../../../core/icons.dart';
 
-PopupMenuItem<String> deleteMessage(BuildContext context) {
+PopupMenuItem<String> deleteMessage(BuildContext context, Messages msg, Testname? widget, User? user, FirebaseService service) {
   return PopupMenuItem<String>(
       value: 'delete',
       child: TextButton(
-        onPressed: (){},
+        onPressed: (){
+          showDialog(context: context,
+            builder: (context) => AlertDialog(
+              title: Text("you are about to delete ${msg.text}"),
+              content: Text("Are you sure? "),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text("Cancel"),
+                ),
+                TextButton(
+                  onPressed: () async {
+                    await service.Deletemessage(
+                      user!.uid,
+                      widget!.receiverId,
+                      msg.messageId!,
+                    );
+                    Navigator.pop(context); // Close dialog after deleting
+                  },
+                  child: Text("Delete", style: Textstyles.deleteStyle,),
+                ),
+              ],
+            )
+          );
+        },
         child: Row(
           children: [
             Text("Delete", style: Textstyles.deletemessage),
