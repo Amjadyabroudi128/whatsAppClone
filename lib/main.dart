1fc8b92dad5fed2e0167afa12e4fe7e480a07273
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-04-15 15:03:45 +0100

    added supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:whatsappclone/colorPicker/ColorPicking.dart';
 import 'package:whatsappclone/core/appTheme.dart';
 import 'package:whatsappclone/features/SignUp/signupScreen.dart';
@@ -8,11 +9,14 @@ import 'package:whatsappclone/features/contacts/contacts.dart';
 import 'features/BottomNavBar/BottomNavBar.dart';
 import 'features/welcomeScreen/welcome.dart';
 import 'firebase_options.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Supabase.initialize(
+    url: "https://txtytzywyezmkwtsfxcy.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4dHl0enl3eWV6bWt3dHNmeGN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3MjUxMTgsImV4cCI6MjA2MDMwMTExOH0.Cn2bSE6ERAadltR4SGFlpZCV5-EJHPXrBKU7mk9w2Wk"
   );
   runApp(const MyApp());
 }
