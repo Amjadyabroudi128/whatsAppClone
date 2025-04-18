46e22d61ea7c1beb915ccd49ac9d790ba27337a4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-04-18 19:36:23 +0100

    pick image

--- lib/utils/pickImage.dart
+++ lib/utils/pickImage.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
+
 File? file;
 String? url;
 Future pickImage() async {
