b20e52d804d5d71089b65fcd13116dbc2227f2ae
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-04-21 14:00:19 +0100

    added style to textBtn

--- lib/components/TextButton.dart
+++ lib/components/TextButton.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class kTextButton extends StatelessWidget {
   final VoidCallback onPressed;
   final Widget child;
-  const kTextButton({super.key, required this.onPressed, required this.child});
+  final ButtonStyle? style;
+  const kTextButton({super.key, required this.onPressed, required this.child, this.style});
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
+      style: style ,
       onPressed: onPressed,
       child: child,
     );
