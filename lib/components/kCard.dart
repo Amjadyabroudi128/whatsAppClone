ea855b7d7b99881e562470e0ae7f7f0e55ceecac
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-04-25 17:49:32 +0100

    card component

--- lib/components/kCard.dart
+++ lib/components/kCard.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 class kCard extends StatelessWidget {
   final Widget? child;
   final ShapeBorder? shape;
-  final Clip? clipBehaviour;
-  const kCard({super.key, this.child, this.shape, this.clipBehaviour});
+  final Clip? clipBehavior;
+  const kCard({super.key, this.child, this.shape, this.clipBehavior});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: child,
       shape: shape,
-      clipBehavior: clipBehaviour,
+      clipBehavior: clipBehavior,
     );
   }
 }
