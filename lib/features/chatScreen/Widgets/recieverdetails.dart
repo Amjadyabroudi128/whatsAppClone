958d38f9513a8844882f43024df28209eec244b5
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-05-02 18:16:28 +0100

    image

--- lib/features/chatScreen/Widgets/recieverdetails.dart
+++ lib/features/chatScreen/Widgets/recieverdetails.dart
@@ -11,13 +11,20 @@ class userDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("${name}"),
+        title: Text("Contact Details"),
         centerTitle: true,
       ),
       body: Center(
         child: Column(
           children: [
-            imageUrl == null || imageUrl!.isEmpty ? Text("This user has no image") : Image.network(imageUrl!)
+            imageUrl == null || imageUrl!.isEmpty ? Container(
+              height: 200,
+              width: 200,
+              child: Image.network(
+                fit: BoxFit.cover,
+                "https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg"
+              ),
+            ): Image.network(imageUrl!),
           ],
         ),
       ),
