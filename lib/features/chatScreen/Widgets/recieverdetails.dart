2919ea232933d53aca3e79b56cd77bb41e9796ac
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-05-04 17:14:25 +0100

    star messages

--- lib/features/chatScreen/Widgets/recieverdetails.dart
+++ lib/features/chatScreen/Widgets/recieverdetails.dart
@@ -3,6 +3,10 @@ import 'package:whatsappclone/components/SizedBox.dart';
 import 'package:whatsappclone/components/TextStyles.dart';
 import 'package:whatsappclone/components/imageNetworkComponent.dart';
 import 'package:whatsappclone/components/kCard.dart';
+import 'package:whatsappclone/components/listTilesOptions.dart';
+
+import '../../../core/MyColors.dart';
+import '../../../core/icons.dart';
 
 class userDetails extends StatelessWidget {
   final String? name;
@@ -14,29 +18,47 @@ class userDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: myColors.BG,
       appBar: AppBar(
+        backgroundColor: myColors.BG,
         title: Text("Contact Details"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            imageUrl == null || imageUrl!.isEmpty ?
-                kimageNet(
-                  src:"https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg",
-                )
-                : kimageNet(src: imageUrl!,),
-            BoxSpacing(myHeight: 9,),
-            Text("${name}", style: Textstyles.recieverName,),
-            BoxSpacing(myHeight: 7,),
-            Text("$email", style: Textstyles.recieverEmail,),
-            bio!.isEmpty ? Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox.shrink()
-            ) : kCard(
-              child: Text("${bio}", style: Textstyles.bioStyle,),
-            )
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Column(
+            children: [
+              imageUrl == null || imageUrl!.isEmpty ?
+                  kimageNet(
+                    src:"https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg",
+                  )
+                  : kimageNet(src: imageUrl!,),
+              BoxSpacing(myHeight: 9,),
+              Text("${name}", style: Textstyles.recieverName,),
+              BoxSpacing(myHeight: 7,),
+              Text("$email", style: Textstyles.recieverEmail,),
+              bio!.isEmpty ? Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox.shrink()
+              ) : kCard(
+                child: Text("${bio}", style: Textstyles.bioStyle,),
+              ),
+              BoxSpacing(myHeight: 5,),
+              kCard(
+                color: Colors.grey,
+                child: Options(
+                  context: context,
+                  leading: Icon(Icons.star_border_outlined),
+                  label: Text("Starred messages"),
+                  trailing: icons.arrowForward,
+                  onTap: (){
+
+                  }
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
