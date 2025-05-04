37ab4740738e4bfdeeeb5c150108e114cfee15d1
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-05-04 17:41:10 +0100

    no star icon color

--- lib/core/icons.dart
+++ lib/core/icons.dart
@@ -31,5 +31,5 @@ class icons {
   static Icon dCam = Icon(Icons.camera_alt_outlined);
   static Icon file = Icon(Icons.file_copy_outlined);
   static Icon star = Icon(Icons.star_border_outlined);
-  static Icon noStar = Icon(Icons.stars, color: Colors.green, size: 90,);
+  static Icon noStar = Icon(Icons.stars, color: myColors.starColor, size: 90,);
 }
