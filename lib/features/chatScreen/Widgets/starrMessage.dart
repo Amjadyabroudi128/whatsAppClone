92a6d6af61292320ca336c94ee01bc85b75b2fd9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-05-05 14:25:11 +0100

    add to firebase

--- lib/features/chatScreen/Widgets/starrMessage.dart
+++ lib/features/chatScreen/Widgets/starrMessage.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/Firebase/FirebaseAuth.dart';
+import 'package:whatsappclone/messageClass/messageClass.dart';
 
 import '../../../components/TextButton.dart';
 import '../../../components/TextStyles.dart';
 import '../../../components/flutterToast.dart';
 import '../../../core/icons.dart';
 
-PopupMenuItem<String> starMessage(BuildContext context) {
+PopupMenuItem<String> starMessage(BuildContext context, Messages msg, User? user) {
+  FirebaseService service = FirebaseService();
   return PopupMenuItem(
     value: "Star",
     child: kTextButton(
@@ -17,6 +21,7 @@ PopupMenuItem<String> starMessage(BuildContext context) {
         ],
       ),
       onPressed: (){
+        service.addToStar(msg.text);
         myToast("Message Starred");
         Navigator.of(context).pop();
       },
