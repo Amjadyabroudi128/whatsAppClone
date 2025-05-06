9a4f8fedc2cb0fb141c4785ee825ddba1d8ccaf8
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-05-06 15:36:42 +0100

    show the messages starred

--- lib/features/chatScreen/userDetails/StarredMessages.dart
+++ lib/features/chatScreen/userDetails/StarredMessages.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:whatsappclone/components/SizedBox.dart';
 import 'package:whatsappclone/components/TextStyles.dart';
+import 'package:whatsappclone/components/kCard.dart';
 
 import '../../../core/icons.dart';
 
@@ -12,6 +16,8 @@ class Starredmessages extends StatefulWidget {
 }
 
 class _StarredmessagesState extends State<Starredmessages> {
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,15 +25,75 @@ class _StarredmessagesState extends State<Starredmessages> {
         title: Text("Starred messages"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            icons.noStar,
-            BoxSpacing(myHeight: 9,),
-            Text("No Starred Messages", style: Textstyles.noStarMessage,),
-            Text("Tap and hold on a message to Star it, to Find it later ", style: TextStyle(fontSize: 16, ),)
-          ],
+      body: SafeArea(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("starred-messages")
+              .doc(auth.currentUser!.email)
+              .collection("messages")
+              .orderBy("timestamp", descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    icons.noStar,
+                    BoxSpacing(myHeight: 9),
+                    Text("No Starred Messages", style: Textstyles.noStarMessage),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30.0),
+                      child: Text(
+                        "Tap and hold on a message to Star it, to find it later.",
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (_, index) {
+                final data = snapshot.data!.docs[index];
+                final message = data["message"] ?? "[No content]";
+                final timestamp = data["timestamp"];
+                DateTime? dateTime;
+
+                if (timestamp is Timestamp) {
+                  dateTime = timestamp.toDate();
+                } else if (timestamp is DateTime) {
+                  dateTime = timestamp;
+                } else {
+                  dateTime = DateTime.now();
+                }
+                String formattedTime = DateFormat.Hm().format(dateTime!);
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: kCard(
+                    color: Colors.grey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(formattedTime),
+                          Text(message)
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
         ),
       ),
     );
