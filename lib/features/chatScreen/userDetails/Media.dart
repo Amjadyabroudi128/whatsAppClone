31cc8c023c30107fea3eac77acf61aa6b18db831
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-05-12 15:20:57 +0100

    if there is no images

--- lib/features/chatScreen/userDetails/Media.dart
+++ lib/features/chatScreen/userDetails/Media.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/components/SizedBox.dart';
+
+import '../../../core/icons.dart';
 
 class myMedia extends StatelessWidget {
   const myMedia({super.key});
@@ -13,9 +17,38 @@ class myMedia extends StatelessWidget {
 
         ],
       ),
-      body: Center(
-        child: Text("Hello "),
-      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("messages").where("image", isNotEqualTo: null).snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+
+          }
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  icons.noImages,
+                  BoxSpacing(myHeight: 10,),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    child: Text(
+                      "no Media",
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  BoxSpacing(myHeight: 9,),
+                  Text('Tap "+" in a Chat to share photos and videos\n with this Contact ',
+                    textAlign: TextAlign.center, style: TextStyle(fontSize: 17, color: Colors.grey), )
+                ],
+              ),
+            );
+          }
+          return Text("Good morning ");
+        },
+      )
     );
   }
 }
