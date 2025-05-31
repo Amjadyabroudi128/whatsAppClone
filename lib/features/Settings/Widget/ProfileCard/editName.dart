b5163c597001e27ddedb619137e42148c5d58d29
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-05-31 14:11:44 +0100

    btm sheet custom

--- lib/features/Settings/Widget/ProfileCard/editName.dart
+++ lib/features/Settings/Widget/ProfileCard/editName.dart
@@ -7,50 +7,47 @@ import '../../../../components/TextStyles.dart';
 import '../../../../core/MyColors.dart';
 
 class editName extends StatelessWidget {
-  const editName({
-    super.key,
-    required this.service,
-    required this.nameController,
-  });
-
   final FirebaseService service;
   final TextEditingController nameController;
+
+  const editName({
+    Key? key,
+    required this.service,
+    required this.nameController,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return FractionallySizedBox(
       heightFactor: 0.94,
-      child: Container(
-        child: Scaffold(
-          backgroundColor: myColors.btmSheet,
-          appBar: AppBar(
-            iconTheme: IconThemeData(
-                color: Colors.white
+      child: Scaffold(
+        backgroundColor: myColors.btmSheet,
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white),
+          backgroundColor: Colors.grey,
+          title: const Text("Edit Your Name", style: TextStyle(color: Colors.white)),
+          actions: [
+            kTextButton(
+              onPressed: () {
+                service.updateName(nameController.text.trim());
+                Navigator.of(context).pop();
+              },
+              child: Text("Save", style: Textstyles.saveBio),
             ),
-            backgroundColor: Colors.grey,
-            title: Text("Edit Your Name", style: TextStyle(color: Colors.white)),
-            actions: [
-              kTextButton(
-                onPressed: (){
-                  service.updateName(nameController.text.trim());
-                  Navigator.of(context).pop();
-                },
-                child: Text("Save", style: Textstyles.saveBio),
-              ),
-            ],
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: kTextField(
-              filled: true,
-              fillColor: Colors.grey,
-              myController: nameController,
-              maxLines: 9,
-              hint: "Edit your name",
-            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: kTextField(
+            filled: true,
+            fillColor: Colors.grey,
+            myController: nameController,
+            maxLines: 9,
+            hint: "Edit your name",
           ),
         ),
       ),
     );
   }
 }
+
