c8c88d917455a8702d7d396c8ef286cd7f346c2b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-06-06 16:16:00 +0100

    chatScreen

--- lib/features/chatScreen/chatScreen.dart
+++ lib/features/chatScreen/chatScreen.dart
@@ -1,40 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:whatsappclone/Firebase/FirebaseAuth.dart';
-import 'package:whatsappclone/colorPicker/colorsList.dart';
-import 'package:whatsappclone/components/TextStyles.dart';
-import 'package:whatsappclone/components/flutterToast.dart';
-import 'package:whatsappclone/components/iconButton.dart';
-import 'package:whatsappclone/core/icons.dart';
-import '../../components/TextField.dart';
-import '../../components/imageNetworkComponent.dart';
+
 import '../../globalState.dart';
 import '../../messageClass/messageClass.dart';
+import '../../Firebase/FirebaseAuth.dart';
+import '../../components/TextField.dart';
+import '../../components/flutterToast.dart';
+import '../../components/iconButton.dart';
+import '../../colorPicker/colorsList.dart';
+import '../../components/TextStyles.dart';
+import '../../core/icons.dart';
 import 'Widgets/messageStream.dart';
-
-import 'Widgets/optionsBtmSheet.dart';
 import 'userDetails/recieverdetails.dart';
+import 'Widgets/optionsBtmSheet.dart';
 
 class Testname extends StatefulWidget {
-  final receiverName;
-  final  receiverId;
-
-  const Testname({super.key, this.receiverName, this.receiverId});
+  final String receiverId;
+  final String receiverName;
+  final String? senderId;
+  final String? msg;
+  const Testname({
+    Key? key,
+    required this.receiverId,
+    required this.receiverName,  this.senderId, this.msg,
+  }) : super(key: key);
 
   @override
   State<Testname> createState() => _TestnameState();
 }
 
 class _TestnameState extends State<Testname> {
-  late final TextEditingController messageController = TextEditingController()
-    ..addListener(() {
-      setState(() {
-        isTextEmpty = messageController.text.trim().isEmpty;
-      });
-    });
+  final TextEditingController messageController = TextEditingController();
   final FirebaseService service = FirebaseService();
-  User? user = FirebaseAuth.instance.currentUser;
+  final User? user = FirebaseAuth.instance.currentUser;
   Messages? _replyMessage;
   bool isTextEmpty = true;
 
@@ -44,6 +43,7 @@ class _TestnameState extends State<Testname> {
       messageController.clear();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<Color>(
@@ -53,40 +53,35 @@ class _TestnameState extends State<Testname> {
         return Scaffold(
           backgroundColor: color,
           appBar: PreferredSize(
-            preferredSize: Size.fromHeight(kToolbarHeight),
+            preferredSize: const Size.fromHeight(kToolbarHeight),
             child: GestureDetector(
-                onTap: () async {
-                  final snapshot = await FirebaseFirestore.instance
-                      .collection('users')
-                      .doc(widget.receiverId)
-                      .get();
+              onTap: () async {
+                final snapshot = await FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(widget.receiverId)
+                    .get();
 
-                  if (snapshot.exists) {
-                    final data = snapshot.data();
-                    final name = data?['name'] ?? 'No Name';
-                    final email = data?['email'] ?? 'No Email';
-                    final image = data?['image'] ?? "";
-                    final bio = data?["bio"] ?? "";
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => userDetails(
-                          name: name,
-                          email: email,
-                          imageUrl: image,
-                          bio: bio,
-                          receiverId: widget.receiverId
-                        ),
+                if (snapshot.exists) {
+                  final data = snapshot.data();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => userDetails(
+                        name: data?['name'] ?? '',
+                        email: data?['email'] ?? '',
+                        imageUrl: data?['image'] ?? '',
+                        bio: data?["bio"] ?? '',
+                        receiverId: widget.receiverId,
                       ),
-                    );
-                  } else {
-                    myToast("there is no user ");
-                  }
-                },
+                    ),
+                  );
+                } else {
+                  myToast("There is no user.");
+                }
+              },
               child: AppBar(
-                centerTitle: false,
                 backgroundColor: color,
-                title: Text(widget.receiverName, style: Textstyles.bioStyle,)
+                title: Text(widget.receiverName, style: Textstyles.bioStyle),
               ),
             ),
           ),
@@ -99,15 +94,13 @@ class _TestnameState extends State<Testname> {
                   user: user,
                   widget: widget,
                   onReply: setReplyMessage,
-                  controller: messageController
+                  controller: messageController,
                 ),
               ),
-
-              // Show reply preview above the input field, if replying
               if (_replyMessage != null)
                 Container(
-                  margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  padding: EdgeInsets.all(8),
+                  margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                  padding: const EdgeInsets.all(8),
                   decoration: BoxDecoration(
                     color: Colors.black,
                     borderRadius: BorderRadius.circular(8),
@@ -123,36 +116,42 @@ class _TestnameState extends State<Testname> {
                         ),
                       ),
                       if (_replyMessage!.image != null && _replyMessage!.image!.isNotEmpty)
-                        Image.network("${_replyMessage!.image}",height: 50, width: 60,),
+                        Image.network(_replyMessage!.image!, height: 50, width: 60),
                       kIconButton(
                         myIcon: icons.Wclose,
-                        onPressed: (){
+                        onPressed: () {
                           setState(() {
                             _replyMessage = null;
                             FocusScope.of(context).unfocus();
                           });
-                        }
+                        },
                       ),
                     ],
                   ),
                 ),
-
               Padding(
-                padding:  EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8.0),
                 child: Row(
                   children: [
                     Expanded(
                       child: kTextField(
                         myController: messageController,
                         hint: "Add a message",
-                        hintStyle: TextStyle(color: textColor, fontSize: 15.7)
+                        hintStyle: TextStyle(color: textColor, fontSize: 15.7),
                       ),
                     ),
                     photoBtmSheet(service: service, widget: widget, textColor: textColor),
                     if (!isTextEmpty)
                       kIconButton(
                         onPressed: () {
-                          service.sendMessage(widget.receiverId, widget.receiverName, messageController.text, null, null, _replyMessage);
+                          service.sendMessage(
+                            widget.receiverId,
+                            widget.receiverName,
+                            messageController.text,
+                            null,
+                            null,
+                            _replyMessage,
+                          );
                           messageController.clear();
                           FocusScope.of(context).unfocus();
                           _replyMessage = null;
@@ -169,6 +168,3 @@ class _TestnameState extends State<Testname> {
     );
   }
 }
-
-
-
