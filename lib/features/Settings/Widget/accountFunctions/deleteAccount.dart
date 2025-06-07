1e8605d8593e93d742c4a08af67a1befa4241b2d
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-06-07 15:38:57 +0100

    delete account

--- lib/features/Settings/Widget/accountFunctions/deleteAccount.dart
+++ lib/features/Settings/Widget/accountFunctions/deleteAccount.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/components/ListTiles.dart';
 import 'package:whatsappclone/components/flutterToast.dart';
+import 'package:whatsappclone/components/listTilesOptions.dart';
 import 'package:whatsappclone/core/icons.dart';
 import '../../../../Firebase/FirebaseAuth.dart';
+import '../../../../components/TextButton.dart';
+import '../../../../components/TextStyles.dart';
 import '../../../../components/iconButton.dart';
 
 class deleteAccount extends StatelessWidget {
@@ -13,17 +16,36 @@ class deleteAccount extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     FirebaseService service =  FirebaseService();
-
-    return kListTile(
-        title: Text("Delete Account",),
-        trailing: kIconButton(
-            onPressed: () async {
-              await service.deleteAccount();
-              Navigator.of(context).pushNamed("welcome");
-              myToast("account deleted");
-            },
-            myIcon: icons.remove
-        )
+    return Options(
+      context: context,
+      label: Text("DeleteAccount"),
+      trailing: icons.remove,
+      onTap: () async {
+        await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("you are about to Delete your account"),
+            content: Text("Are you sure you wanna leave us? "),
+              actions: [
+              kTextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+                FocusScope.of(context).unfocus();
+        },
+                child: Text("Cancel"),
+              ),
+                kTextButton(
+                  onPressed: () async {
+                    Navigator.of(context).pushNamed("welcome");
+                    await service.deleteAccount();
+                    myToast("account deleted");
+                  },
+                  child: Text("Delete", style: Textstyles.deleteStyle,),
+                ),
+              ]
+          )
+        );
+      }
     );
   }
 }
