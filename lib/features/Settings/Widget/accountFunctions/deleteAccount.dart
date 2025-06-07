05872b9b883ca58c493371511100960c4b053806
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-06-07 15:18:41 +0100

    delete account completely

--- lib/features/Settings/Widget/accountFunctions/deleteAccount.dart
+++ lib/features/Settings/Widget/accountFunctions/deleteAccount.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/components/ListTiles.dart';
+import 'package:whatsappclone/components/flutterToast.dart';
 import 'package:whatsappclone/core/icons.dart';
+import '../../../../Firebase/FirebaseAuth.dart';
 import '../../../../components/iconButton.dart';
 
 class deleteAccount extends StatelessWidget {
@@ -10,10 +12,16 @@ class deleteAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FirebaseService service =  FirebaseService();
+
     return kListTile(
         title: Text("Delete Account",),
         trailing: kIconButton(
-            onPressed: (){} ,
+            onPressed: () async {
+              await service.deleteAccount();
+              Navigator.of(context).pushNamed("welcome");
+              myToast("account deleted");
+            },
             myIcon: icons.remove
         )
     );
