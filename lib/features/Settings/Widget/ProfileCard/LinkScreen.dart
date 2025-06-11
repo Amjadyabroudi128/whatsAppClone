3434ae43e0682e97681dafb8989e62246c8a8685
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-06-11 17:33:10 +0100

    link screen

---
+++ lib/features/Settings/Widget/ProfileCard/LinkScreen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../Firebase/FirebaseAuth.dart';
+import '../../../../components/SizedBox.dart';
+import '../../../../components/TextButton.dart';
+import '../../../../components/TextField.dart';
+import '../../../../core/MyColors.dart';
+
+class LinksScreen extends StatelessWidget {
+  final TextEditingController linkController;
+
+  const LinksScreen({super.key, required this.linkController, required link});
+
+  @override
+  Widget build(BuildContext context) {
+    FirebaseService service = FirebaseService();
+    return FractionallySizedBox(
+      heightFactor: 0.94,
+      child: Scaffold(
+        backgroundColor: myColors.btmSheet,
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          title: Text("Add link"),
+          actions: [
+            kTextButton(
+              child: Text("Save"),
+              onPressed: ()async {
+                await service.addLink(linkController.text.trim());
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Instagram UserName", style: TextStyle(color: Colors.white),),
+              BoxSpacing(myHeight: 10,),
+              kTextField(
+                filled: true,
+                fillColor: Colors.grey,
+                myController: linkController,
+                maxLines: 1,
+                hint: "${linkController.text.isEmpty ? "Link" : linkController}",
+              ),
+              BoxSpacing(myHeight: 10,),
+              Text("adding isntagram to your profile will make it visible", style: TextStyle(fontSize: 13, color: CupertinoColors.systemGrey),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/Settings/Widget/ProfileCard/nameCard.dart
+++ lib/features/Settings/Widget/ProfileCard/nameCard.dart
@@ -18,6 +18,7 @@ import '../../../../components/TextButton.dart';
 import '../../../../components/TextField.dart';
 import '../../../../core/icons.dart';
 import 'ImageFullScreen.dart';
+import 'LinkScreen.dart';
 import 'editName.dart';
 import 'imageSheet.dart';
 import 'imageWidget.dart';
@@ -179,50 +180,4 @@ class _nameCardState extends State<nameCard> {
 
 }
 
-class LinksScreen extends StatelessWidget {
-  final TextEditingController linkController;
-
-  const LinksScreen({super.key, required this.linkController, required link});
-
-  @override
-  Widget build(BuildContext context) {
-    FirebaseService service = FirebaseService();
-    return FractionallySizedBox(
-      heightFactor: 0.94,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Add link"),
-          actions: [
-            kTextButton(
-              child: Text("Save"),
-              onPressed: ()async {
-                await service.addLink(linkController.text.trim());
-                Navigator.of(context).pop();
-              },
-            )
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Instagram UserName"),
-              BoxSpacing(myHeight: 10,),
-              kTextField(
-                filled: true,
-                fillColor: Colors.grey,
-                myController: linkController,
-                maxLines: 1,
-                hint: "${linkController.text.isEmpty ? "Link" : linkController}",
-              ),
-              BoxSpacing(myHeight: 10,),
-              Text("adding isntagram to your profile will make it visible", style: TextStyle(fontSize: 13, color: CupertinoColors.systemGrey),)
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
 
