551d07bffa25b9e930976fe6fc1e910579742be7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-06-11 16:33:38 +0100

    edit button

--- lib/features/Settings/Widget/ProfileCard/editBio.dart
+++ lib/features/Settings/Widget/ProfileCard/editBio.dart
@@ -21,11 +21,6 @@ class _EditBioState extends State<EditBio> {
   final TextEditingController bioController = TextEditingController();
   FirebaseService service = FirebaseService();
   @override
-  void initState() {
-    super.initState();
-    bioController.text = widget.bio ?? '';
-  }
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: myColors.btmSheet,
@@ -36,20 +31,7 @@ class _EditBioState extends State<EditBio> {
         backgroundColor: Colors.grey,
         title: Text("Edit Your Bio", style: TextStyle(color: Colors.white)),
         actions: [
-          kTextButton(
-            onPressed: () async {
-              String newBio = bioController.text.trim();
-              if(newBio.isEmpty) {
-                myToast("add someThing to your bio");
-              } else if (newBio == (widget.bio ?? '')) {
-                myToast("Nothing changed in the bio");
-              } else {
-                await service.updateBio(newBio);
-                Navigator.of(context).pop();
-              }
-            },
-            child: Text("Save", style: Textstyles.saveBio),
-          ),
+          editButton()
         ],
       ),
       body: Padding(
@@ -62,6 +44,22 @@ class _EditBioState extends State<EditBio> {
           hint: "Edit your Bio",
         ),
       ),
+    );
+  }
+  editButton () {
+    return kTextButton(
+      onPressed: () async {
+        String newBio = bioController.text.trim();
+        if(newBio.isEmpty) {
+          myToast("add someThing to your bio");
+        } else if (newBio == (widget.bio ?? '')) {
+          myToast("Nothing changed in the bio");
+        } else {
+          await service.updateBio(newBio);
+          Navigator.of(context).pop();
+        }
+      },
+      child: Text("Save", style: Textstyles.saveBio),
     );
   }
 }
