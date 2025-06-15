2ae755d65e4ebd39890422257b19fc204818a369
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-06-15 14:35:54 +0100

    select message

---
+++ lib/features/chatScreen/Widgets/selectMessage.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../components/TextButton.dart';
+import '../../../components/TextStyles.dart';
+import '../../../core/icons.dart';
+import '../../../messageClass/messageClass.dart';
+
+PopupMenuItem<String> selectMessage(BuildContext context, Messages msg, bool isEditing,
+    VoidCallback? onToggleEdit, Set<String> selectedMessages, VoidCallback onUpdateState) {
+  return PopupMenuItem(
+      value: "select",
+      child:kTextButton(
+        child: Row(
+          children: [
+            Text("Select",style: Textstyles.copyMessage,),
+            Spacer(),
+            icons.selectIcon
+          ],
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+          onUpdateState();
+        },
+      )
+  );
+}
