a8f15a86fd9998899ac3c95f5668049355a37502
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-06-17 20:17:10 +0100

    text theme

--- lib/core/appTheme.dart
+++ lib/core/appTheme.dart
@@ -21,6 +21,7 @@ import 'package:whatsappclone/core/MyColors.dart';
      borderRadius: BorderRadius.circular(10.0),
    );
   static final ThemeData appTheme = ThemeData().copyWith(
+    scaffoldBackgroundColor: Colors.white,
     appBarTheme: AppBarTheme(
       centerTitle: true,
       iconTheme: IconThemeData(
@@ -113,6 +114,23 @@ import 'package:whatsappclone/core/MyColors.dart';
         style: IconButton.styleFrom(
           foregroundColor: Colors.white,
         )
+    ),
+    textTheme: const TextTheme(
+      displayLarge: TextStyle(color: Colors.white),
+      displayMedium: TextStyle(color: Colors.white),
+      displaySmall: TextStyle(color: Colors.white),
+      bodyLarge: TextStyle(color: Colors.white),
+      bodyMedium:TextStyle(color: Colors.white),
+      bodySmall: TextStyle(color: Colors.white),
+      headlineLarge: TextStyle(color: Colors.white),
+      headlineMedium: TextStyle(color: Colors.white),
+      headlineSmall: TextStyle(color: Colors.white),
+      titleLarge: TextStyle(color: Colors.white),
+      titleMedium:TextStyle(color: Colors.white) ,
+      titleSmall: TextStyle(color: Colors.white),
+      labelLarge: TextStyle(color: Colors.white),
+      labelMedium:TextStyle(color: Colors.white) ,
+      labelSmall: TextStyle(color: Colors.white),
     ),
     scaffoldBackgroundColor: myColors.darkCard,
   );
