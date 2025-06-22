fd41f3b4bf89d71c57db61ff31c25a608998e8ae
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-06-22 18:43:35 +0100

    unfocus

--- lib/features/Settings/Widget/ProfileCard/LinkScreen.dart
+++ lib/features/Settings/Widget/ProfileCard/LinkScreen.dart
@@ -18,41 +18,46 @@ class LinksScreen extends StatelessWidget {
     FirebaseService service = FirebaseService();
     return FractionallySizedBox(
       heightFactor: 0.94,
-      child: Scaffold(
-        appBar: AppBar(
+      child: GestureDetector(
+        onTap: (){
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          appBar: AppBar(
 
-          title: Text("Add link"),
-          actions: [
-            kTextButton(
-              child: Text("Save", style: Textstyles.saveBio,),
-              onPressed: ()async {
-                await service.addLink(linkController.text.trim());
-                Navigator.of(context).pop();
-              },
-            )
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Instagram Link",),
-              BoxSpacing(myHeight: 10,),
-              kTextField(
-                enable: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                        color: Colors.transparent
-                    )
-                ),
-                filled: true,
-                myController: linkController,
-                maxLines: 2,
-                hint: "${linkController.text.isEmpty ? "Link" : linkController}",
-              ),
-              BoxSpacing(myHeight: 13,),
-              Text("adding isntagram to your profile will make it visible", style: Textstyles.insta,)
+            title: Text("Add link"),
+            actions: [
+              kTextButton(
+                child: Text("Save", style: Textstyles.saveBio,),
+                onPressed: ()async {
+                  await service.addLink(linkController.text.trim());
+                  Navigator.of(context).pop();
+                },
+              )
             ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Instagram Link",),
+                BoxSpacing(myHeight: 10,),
+                kTextField(
+                  enable: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                          color: Colors.transparent
+                      )
+                  ),
+                  filled: true,
+                  myController: linkController,
+                  maxLines: 2,
+                  hint: "${linkController.text.isEmpty ? "Link" : linkController}",
+                ),
+                BoxSpacing(myHeight: 13,),
+                Text("adding isntagram to your profile will make it visible", style: Textstyles.insta,)
+              ],
+            ),
           ),
         ),
       ),
