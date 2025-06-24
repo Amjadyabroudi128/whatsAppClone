4c8d0234114a6a9293a1192f243d5347911ba0f3
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-06-24 13:00:50 +0100

    reply email is you?

--- lib/features/chatScreen/Widgets/alignMessages.dart
+++ lib/features/chatScreen/Widgets/alignMessages.dart
@@ -282,7 +282,7 @@ class _messagesAlignState extends State<messagesAlign> {
                                         crossAxisAlignment: CrossAxisAlignment.start,
                                         children: [
                                           Text(
-                                            "${msg.replyTo!.senderEmail}",
+                                            "${msg.replyTo!.senderEmail == FirebaseAuth.instance.currentUser!.email ? "You" : msg.replyTo!.senderEmail}",
                                             style: TextStyle(
                                               color: msg.replyTo!.senderEmail == FirebaseAuth.instance.currentUser!.email
                                                   ? Colors.orange.shade800
