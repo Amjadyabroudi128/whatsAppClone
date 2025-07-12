382e07caed108792c89f581fc6f75d03347034c9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-07-12 18:18:21 +0100

    subtitle

--- lib/components/ListTiles.dart
+++ lib/components/ListTiles.dart
@@ -5,11 +5,13 @@ class kListTile extends StatelessWidget {
   final VoidCallback? onTap;
   final Widget? trailing;
   final Widget? leading;
-  const kListTile({super.key, this.title, this.onTap, this.trailing, this.leading});
+  final Widget? subtitle;
+  const kListTile({super.key, this.title, this.onTap, this.trailing, this.leading, this.subtitle});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      subtitle: subtitle,
       title: title,
       onTap: onTap,
       trailing: trailing,
