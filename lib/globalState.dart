37a15076eb9e703817bf6eb255fce518fd749446
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-07-18 19:40:36 +0100

    current receiver id

--- lib/globalState.dart
+++ lib/globalState.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 // Global ValueNotifier for the selected color
 final ValueNotifier<Color> selectedThemeColor = ValueNotifier<Color>(Colors.white);
+ValueNotifier<String?> currentReceiverId = ValueNotifier(null);
+
