622ef650d6795c2e097fce294d848164a3301ed5
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-07-20 15:07:25 +0100

    message read icon

--- lib/core/icons.dart
+++ lib/core/icons.dart
@@ -50,4 +50,5 @@ class icons {
   static Icon selectIcon = Icon(Icons.check_circle_outline, color: myColors.labelClr);
   static Icon search = Icon(Icons.search);
   static Icon instagram = Icon(FontAwesomeIcons.instagram);
+  static Icon messageRead = Icon(Icons.done_all, color: Colors.blue[400],);
 }
