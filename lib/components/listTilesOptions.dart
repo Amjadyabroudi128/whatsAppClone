e2bfa5d28ddfef7a202533390f2a6108e766bb82
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-07-24 14:37:11 +0100

    key

--- lib/components/listTilesOptions.dart
+++ lib/components/listTilesOptions.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 import 'ListTiles.dart';
 
 Widget Options({
-  required BuildContext context, Widget? leading, Widget? trailing, Widget? label, VoidCallback? onTap, Widget? subtitle}) {
+  required BuildContext context, Widget? leading, Widget? trailing, Widget? label, VoidCallback? onTap, Widget? subtitle, Key? key}) {
   return kListTile(
     subtitle: subtitle,
     onTap: onTap,
     leading: leading,
     trailing: trailing,
     title: label,
+    key: key,
   );
 }
