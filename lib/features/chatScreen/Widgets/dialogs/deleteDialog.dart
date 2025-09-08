1c48b253db94da8057c147329646d442017dd2ac
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-09-08 18:01:15 +0100

    delete dialog

--- lib/features/chatScreen/Widgets/dialogs/deleteDialog.dart
+++ lib/features/chatScreen/Widgets/dialogs/deleteDialog.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import '../../../../Firebase/FirebaseAuth.dart';
 import '../../../../components/SizedBox.dart';
 import '../../../../components/TextButton.dart';
@@ -12,37 +11,70 @@ import '../../chatScreen.dart';
 
 class deleteDialog extends StatelessWidget {
   const deleteDialog({
-    super.key,  required this.msg, required this.service, this.Test, this.user,
+    super.key,
+    required this.msg,
+    required this.service,
+    this.Test,
+    this.user,
   });
+
   final Messages msg;
   final FirebaseService service;
   final Testname? Test;
   final User? user;
+
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: msg.image != null && msg.image!.isNotEmpty ?
-      Column(
+    // Determine message type and content to display
+    String messageType;
+    Widget titleWidget;
+
+    if (msg.text.isNotEmpty) {
+      messageType = "text";
+      titleWidget = Text("You are about to delete \"${msg.text}\"");
+    } else if (msg.image != null && msg.image!.isNotEmpty) {
+      messageType = "image";
+      titleWidget = Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Text("You are about to delete"),
-          BoxSpacing(),
+          const Text("You are about to delete this image"),
+          const BoxSpacing(),
           kimageNet(
             src: msg.image!,
           ),
         ],
-      ) :Text("You are about to delete ${msg.text}"),
-      content: Text("Are you sure? "),
+      );
+    } else if (msg.file != null && msg.file!.isNotEmpty) {
+      messageType = "file";
+      titleWidget = Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text("You are about to delete this file"),
+          const BoxSpacing(),
+          kimageNet(
+            src: msg.file!,
+          ),
+        ],
+      );
+    } else {
+      messageType = "unknown";
+      titleWidget = const Text("You are about to delete this message");
+    }
+
+    return AlertDialog(
+      title: titleWidget,
+      content: const Text("Are you sure?"),
       actions: [
         kTextButton(
-          onPressed: (){
+          onPressed: () {
             Navigator.of(context).pop();
             FocusScope.of(Navigator.of(context).context).unfocus();
           },
-          child: Text("Cancel"),
+          child: const Text("Cancel"),
         ),
         kTextButton(
           onPressed: () async {
+            FocusScope.of(Navigator.of(context).context).unfocus();
             Navigator.of(context).pop();
             myToast("Message Successfully Deleted");
             FocusScope.of(Navigator.of(context).context).unfocus();
@@ -51,10 +83,9 @@ class deleteDialog extends StatelessWidget {
               msg.receiverId!,
               msg.messageId!,
             );
-            FocusScope.of(Navigator.of(context).context).unfocus();
             await service.deleteStar(msg);
-            FocusScope.of(Navigator.of(context).context).unfocus();
-            },
+
+          },
           child: Text("Delete", style: Textstyles.deleteStyle,),
         ),
       ],
