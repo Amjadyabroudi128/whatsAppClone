2e0b21e9a4d8d45d12469743722ae4a6d725db2f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-09-15 16:56:11 +0100

    thanks screen

---
+++ lib/features/Settings/Widget/issueReport/Widgets/thanksScreen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ThanksScreen extends StatefulWidget {
+  const ThanksScreen({super.key});
+
+  @override
+  State<ThanksScreen> createState() => _ThanksScreenState();
+}
+
+class _ThanksScreenState extends State<ThanksScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Wait 4 seconds, then pop back.
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.of(context).pop();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      // Prevent accidental back until auto-pop
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.check_circle_outline, size: 72),
+              SizedBox(height: 16),
+              Text(
+                "Thanks!",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                "We’ve received your issue. We’ll take a look shortly.",
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
