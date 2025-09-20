d911fa0fd1683b496f373c467ba3c8e9a6aa32cb
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-09-20 12:28:58 +0100

    flutter analyze

--- lib/features/SignInScreen/signIn.dart
+++ lib/features/SignInScreen/signIn.dart
@@ -1,9 +1,9 @@
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/components/SizedBox.dart';
 import 'package:whatsappclone/components/TextButton.dart';
 import 'package:whatsappclone/components/padding.dart';
-import 'package:whatsappclone/core/MyColors.dart';
 import '../../Firebase/FirebaseAuth.dart';
 import '../SignUp/Widgets/emailTextField.dart';
 import '../SignUp/Widgets/nameTextfield.dart';
@@ -34,29 +34,29 @@ class _SignInscreenState extends State<SignInscreen> {
       child: Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
-          title: Text("Sign in"),
+          title: const Text("Sign in"),
         ),
         body: myPadding(
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               emailField(myEmail: myEmail),
-              BoxSpacing(myHeight: 20,),
+              const BoxSpacing(myHeight: 20,),
               nameField(name: name),
-              BoxSpacing(myHeight: 20,),
+              const BoxSpacing(myHeight: 20,),
               passField(pass: pass),
               Padding(
-                padding: EdgeInsets.only(right: 1, top: 3),
+                padding: const EdgeInsets.only(right: 1, top: 3),
                 child: kTextButton(
                   onPressed: (){
                     Navigator.of(context).pushNamed("passReset");
                   },
-                  child: Text("Forgotten Password?"),
+                  child: const Text("Forgotten Password?"),
                 ),
               ),
-              BoxSpacing(myHeight: 19,),
+              const BoxSpacing(myHeight: 19,),
               Center(
                 child: siginIn(firebase: firebase, myEmail: myEmail, pass: pass, user: user, name: name,),
               ),
