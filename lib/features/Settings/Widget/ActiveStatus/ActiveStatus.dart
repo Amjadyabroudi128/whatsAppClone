14b4e0403b319ef3ab64150ece83119283ad538b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-10-12 15:57:30 +0100

    online visibility

--- lib/features/Settings/Widget/ActiveStatus/ActiveStatus.dart
+++ lib/features/Settings/Widget/ActiveStatus/ActiveStatus.dart
@@ -5,7 +5,7 @@ import 'package:whatsappclone/components/flutterToast.dart';
 import 'package:whatsappclone/components/kCard.dart';
 import 'package:whatsappclone/components/listTilesOptions.dart';
 import 'package:whatsappclone/core/TextStyles.dart';
-
+import '../../../../Firebase/FirebaseAuth.dart';
 import '../../../../core/icons.dart';
 
 class ActiveStatus extends StatefulWidget {
@@ -17,7 +17,7 @@ class ActiveStatus extends StatefulWidget {
 
 class _ActiveStatusState extends State<ActiveStatus> {
   String selectedOption = 'Everyone'; // Default selection
-  // bool isSelected = false;
+  final FirebaseService service = FirebaseService();
 
   @override
   Widget build(BuildContext context) {
@@ -34,47 +34,48 @@ class _ActiveStatusState extends State<ActiveStatus> {
           children: [
             // Description text
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4, ),
+              padding: const EdgeInsets.symmetric(horizontal: 4),
               child: Text(
-                "Who can see when I'm online",
-                style: Textstyles.whoOnline
+                  "Who can see when I'm online",
+                  style: Textstyles.whoOnline
               ),
             ),
             const BoxSpacing(myHeight: 16),
-              kCard(
-                child: Column(
-                  children: [
-                    Options(
-                      context: context,
-                      label: const Text("Everyone"),
-                      onTap: () {
-                        setState(() {
-                          selectedOption = "Everyone";
-                          myToast("Your status is now visible");
-                        });
-                      },
-                      trailing: selectedOption == "Everyone"
-                          ? icons.onlineStatus
-                          : const SizedBox.shrink(),
-                    ),
-                    const divider(),
-                    Options(
-                      context: context,
-                      label: const Text("Nobody"),
-                      onTap: () {
-                        setState(() {
-                          selectedOption = "Nobody";
-                          myToast("Only you can see your status ");
-                        });
-                      },
-                      trailing: selectedOption == "Nobody"
-                          ? icons.onlineStatus
-                          : const SizedBox.shrink(),
-                    ),
-                  ],
-                ),
+            kCard(
+              child: Column(
+                children: [
+                  Options(
+                    context: context,
+                    label: const Text("Everyone"),
+                    onTap: () async {
+                      await service.setOnlineVisibility("Everyone");
+                      setState(() {
+                        selectedOption = "Everyone";
+                      });
+                      myToast("Your status is now visible");
+                    },
+                    trailing: selectedOption == "Everyone"
+                        ? icons.onlineStatus
+                        : const SizedBox.shrink(),
+                  ),
+                  const divider(),
+                  Options(
+                    context: context,
+                    label: const Text("Nobody"),
+                    onTap: () async {
+                      await service.setOnlineVisibility("Nobody");
+                      setState(() {
+                        selectedOption = "Nobody";
+                      });
+                      myToast("Only you can see your status ");
+                    },
+                    trailing: selectedOption == "Nobody"
+                        ? icons.onlineStatus
+                        : const SizedBox.shrink(),
+                  ),
+                ],
               ),
-
+            ),
             const BoxSpacing(myHeight: 24),
           ],
         ),
