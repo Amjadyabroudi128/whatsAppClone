319529ecdefacb8a440d0091051b1119c1cdbf7b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-10-26 20:06:28 +0000

    color

--- lib/components/ElevatedBtn.dart
+++ lib/components/ElevatedBtn.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 class kElevatedBtn extends StatelessWidget {
   final VoidCallback? onPressed;
   final Widget? child;
-  const kElevatedBtn({super.key, this.onPressed, this.child});
+  final WidgetStatePropertyAll<Color>? color;
+  const kElevatedBtn({super.key, this.onPressed, this.child, this.color});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
+      style: ButtonStyle(
+        backgroundColor: color
+      ),
       child: child,
     );
   }
