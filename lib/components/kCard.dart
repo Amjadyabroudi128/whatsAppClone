03a3c4c860f996d366b9f408c3bbc978dc018b01
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-11-03 14:16:01 +0000

    elevation

--- lib/components/kCard.dart
+++ lib/components/kCard.dart
@@ -5,15 +5,17 @@ class kCard extends StatelessWidget {
   final ShapeBorder? shape;
   final Clip? clipBehavior;
   final Color? color;
-  const kCard({super.key, this.child, this.shape, this.clipBehavior, this.color});
+  final double? elevation;
+  const kCard({super.key, this.child, this.shape, this.clipBehavior, this.color, this.elevation});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: elevation,
       color: color,
-      child: child,
       shape: shape,
       clipBehavior: clipBehavior,
+      child: child,
     );
   }
 }
