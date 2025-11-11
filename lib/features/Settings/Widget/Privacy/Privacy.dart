445c39d4dfe8d2f59e63bcaceb54ea4ddb04ab48
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-11-11 14:44:11 +0000

    bio

--- lib/features/Settings/Widget/Privacy/Privacy.dart
+++ lib/features/Settings/Widget/Privacy/Privacy.dart
@@ -50,79 +50,53 @@ class _ActiveStatusState extends State<PrivacyScreen> {
                 trailing: icons.arrowForward(context),
               ),
             ),
-            // kCard(
-            //   child: Column(
-            //     children: [
-            //       Options(
-            //         context: context,
-            //         label: const Text("Everyone"),
-            //         onTap: () async {
-            //           await service.setOnlineVisibility("Everyone");
-            //           setState(() {
-            //             selectedOption = "Everyone";
-            //           });
-            //           myToast("Your status is now visible");
-            //         },
-            //         trailing: selectedOption == "Everyone"
-            //             ? icons.onlineStatus
-            //             : const SizedBox.shrink(),
-            //       ),
-            //       const divider(),
-            //       Options(
-            //         context: context,
-            //         label: const Text("Nobody"),
-            //         onTap: () async {
-            //           await service.setOnlineVisibility("Nobody");
-            //           setState(() {
-            //             selectedOption = "Nobody";
-            //           });
-            //           myToast("Only you can see your status ");
-            //         },
-            //         trailing: selectedOption == "Nobody"
-            //             ? icons.onlineStatus
-            //             : const SizedBox.shrink(),
-            //       ),
-            //
-            //     ],
-            //   ),
-            // ),
-            const BoxSpacing(myHeight: 24),
-             Text("Who can see My Bio",style: Textstyles.whoOnline),
-      kCard(
-        child: Column(
-          children: [
-            Options(
-              context: context,
-              label: const Text("Everyone"),
-              onTap: () async {
-                await service.bioVisibility("Everyone");
-                setState(() {
-                  selectedBio = "Everyone";
-                });
-                myToast("Everyone can see your bio");
-              },
-              trailing: selectedBio == "Everyone"
-                  ? icons.onlineStatus
-                  : const SizedBox.shrink(),
-            ),
-            const divider(),
-            Options(
-              context: context,
-              label: const Text("Nobody"),
-              onTap: () async {
-                await service.bioVisibility("Nobody");
-                setState(() {
-                  selectedBio = "Nobody";
-                });
-                myToast("Only you can see your bio");
-              },
-              trailing: selectedBio == "Nobody"
-                  ? icons.onlineStatus
-                  : const SizedBox.shrink(),
-            ),
-          ],
-        ),
-      )
+            const BoxSpacing(myHeight: 6),
+             kCard(
+               child: Options(
+                 context: context,
+                 onTap: (){
+                   
+                 },
+                 trailing: icons.arrowForward(context),
+                 label: Text("My Bio")
+               ),
+             )
+             // Text("Who can see My Bio",style: Textstyles.whoOnline),
+      // kCard(
+      //   child: Column(
+      //     children: [
+      //       Options(
+      //         context: context,
+      //         label: const Text("Everyone"),
+      //         onTap: () async {
+      //           await service.bioVisibility("Everyone");
+      //           setState(() {
+      //             selectedBio = "Everyone";
+      //           });
+      //           myToast("Everyone can see your bio");
+      //         },
+      //         trailing: selectedBio == "Everyone"
+      //             ? icons.onlineStatus
+      //             : const SizedBox.shrink(),
+      //       ),
+      //       const divider(),
+      //       Options(
+      //         context: context,
+      //         label: const Text("Nobody"),
+      //         onTap: () async {
+      //           await service.bioVisibility("Nobody");
+      //           setState(() {
+      //             selectedBio = "Nobody";
+      //           });
+      //           myToast("Only you can see your bio");
+      //         },
+      //         trailing: selectedBio == "Nobody"
+      //             ? icons.onlineStatus
+      //             : const SizedBox.shrink(),
+      //       ),
+      //     ],
+      //   ),
+      // )
       ],
         ),
       ),
