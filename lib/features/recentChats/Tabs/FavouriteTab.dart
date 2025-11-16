c12a57860e0a9065007f8183679944bb223f229c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-11-16 16:22:36 +0000

    favourite tab

--- lib/features/recentChats/Tabs/FavouriteTab.dart
+++ lib/features/recentChats/Tabs/FavouriteTab.dart
@@ -1,24 +1,105 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../../../components/SizedBox.dart';
+import '../../../components/kCard.dart';
+import '../../../components/listTilesOptions.dart';
+import '../../../core/TextStyles.dart';
+import '../../../core/icons.dart';
+class FavouriteTab extends StatelessWidget {
+  const FavouriteTab({
+    super.key,
+    required this.user,
+  });
 
-import '../../../Firebase/FirebaseAuth.dart';
-class FavouriteTab extends StatefulWidget {
-  const FavouriteTab({super.key});
+  final User? user;
 
-  @override
-  State<FavouriteTab> createState() => _FavouriteTabState();
-}
-
-class _FavouriteTabState extends State<FavouriteTab> {
-  final FirebaseService service = FirebaseService();
-  final User? user = FirebaseAuth.instance.currentUser;
-
-  String getChatRoomId(String userId1, String userId2) {
-    final ids = [userId1, userId2]..sort();
-    return ids.join("_");
-  }
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Favourite"));
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection("Favourites")
+          .doc(user!.email)
+          .collection("myFavourites")
+          .snapshots(),
+      builder: (context, snap){
+        if (snap.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snap.hasError) {
+          return Center(
+            child: Text(
+              'Something went wrong\n${snap.error}',
+              textAlign: TextAlign.center,
+            ),
+          );
+        }
+        if (!snap.hasData || snap.data!.docs.isEmpty) {
+          return Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icons.myFavourite(context, size: 80),
+                const BoxSpacing(myHeight: 10),
+                Text("No Favourite yet", style: Textstyles.noStarMessage),
+                const BoxSpacing(myHeight: 10),
+                const Text(
+                  "add user to favourite to see it here ",
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          );
+        }
+        final favourites = snap.data!.docs;
+
+        return Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Favourites",
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: favourites.length,
+                  itemBuilder: (context, index) {
+                    final favData =
+                    favourites[index].data();
+                    final name = favData['name'] ?? 'Unknown';
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            child: kCard(
+                              child: Options(
+                                context: context,
+                                label: Text(name),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+
+      },
+    );
   }
 }
