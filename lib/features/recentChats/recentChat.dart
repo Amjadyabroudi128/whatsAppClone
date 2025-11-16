5e9c6005d42d2dae60a480d7a3a3ddceacf3d7c3
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-11-16 16:22:45 +0000

    favourite tab

--- lib/features/recentChats/recentChat.dart
+++ lib/features/recentChats/recentChat.dart
@@ -138,91 +138,7 @@ class _RecentChatsScreenState extends State<RecentChatsScreen> {
           // List area
           Expanded(
             child: (_selectedTab == 1 ? const UnreadTab()
-                : _selectedTab == 2 ? StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection("Favourites")
-                      .doc(user!.email)
-                      .collection("myFavourites")
-                      .snapshots(),
-                  builder: (context, snap){
-                    if (snap.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    if (snap.hasError) {
-                      return Center(
-                        child: Text(
-                          'Something went wrong\n${snap.error}',
-                          textAlign: TextAlign.center,
-                        ),
-                      );
-                    }
-                    if (!snap.hasData || snap.data!.docs.isEmpty) {
-                      return Center(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            icons.myFavourite(context, size: 80),
-                            const BoxSpacing(myHeight: 10),
-                            Text("No Favourite yet", style: Textstyles.noStarMessage),
-                            const BoxSpacing(myHeight: 10),
-                            const Text(
-                              "add user to favourite to see it here ",
-                              style: TextStyle(fontSize: 16),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                      );
-                    }
-                    final favourites = snap.data!.docs;
-
-                    return Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              "Favourites",
-                              style: TextStyle(color: Colors.grey, fontSize: 16),
-                            ),
-                          ),
-                          Expanded(
-                            child: ListView.builder(
-                              itemCount: favourites.length,
-                              itemBuilder: (context, index) {
-                                final favData =
-                                favourites[index].data();
-                                final name = favData['name'] ?? 'Unknown';
-                                return Padding(
-                                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                                  child: Row(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Flexible(
-                                        child: kCard(
-                                          child: Options(
-                                            context: context,
-                                            label: Text(name),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-
-                  },
-                )
+                : _selectedTab == 2 ? FavouriteTab(user: user)
                 : _buildChatsList()),
           ),
         ],
@@ -643,6 +559,104 @@ class _RecentChatsScreenState extends State<RecentChatsScreen> {
               hasUnreadMessages ? MyColors.familyText : MyColors.unread,
           child: hasUnreadMessages ? icons.read : icons.unread,
         );
+      },
+    );
+  }
+}
+
+class FavouriteTab extends StatelessWidget {
+  const FavouriteTab({
+    super.key,
+    required this.user,
+  });
+
+  final User? user;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection("Favourites")
+          .doc(user!.email)
+          .collection("myFavourites")
+          .snapshots(),
+      builder: (context, snap){
+        if (snap.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snap.hasError) {
+          return Center(
+            child: Text(
+              'Something went wrong\n${snap.error}',
+              textAlign: TextAlign.center,
+            ),
+          );
+        }
+        if (!snap.hasData || snap.data!.docs.isEmpty) {
+          return Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icons.myFavourite(context, size: 80),
+                const BoxSpacing(myHeight: 10),
+                Text("No Favourite yet", style: Textstyles.noStarMessage),
+                const BoxSpacing(myHeight: 10),
+                const Text(
+                  "add user to favourite to see it here ",
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          );
+        }
+        final favourites = snap.data!.docs;
+
+        return Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Favourites",
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: favourites.length,
+                  itemBuilder: (context, index) {
+                    final favData =
+                    favourites[index].data();
+                    final name = favData['name'] ?? 'Unknown';
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            child: kCard(
+                              child: Options(
+                                context: context,
+                                label: Text(name),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+
       },
     );
   }
