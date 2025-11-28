bbef80f2afd1e1540c31abb29f732a5f3f068867
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-11-28 18:25:10 +0000

    shorthands

--- lib/core/consts.dart
+++ lib/core/consts.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 import 'MyColors.dart';
 import 'appTheme.dart';
 
-EdgeInsetsGeometry containerPadding = const EdgeInsets.all(10);
-EdgeInsetsGeometry containermargin = const EdgeInsets.symmetric(vertical: 5, horizontal: 10);
+EdgeInsetsGeometry containerPadding = const .all(10);
+EdgeInsetsGeometry containermargin = const .symmetric(vertical: 5, horizontal: 10);
 Decoration containerDecoration({Color? color, BorderRadiusGeometry? borderRadius}) {
   return BoxDecoration(
     color: color,
@@ -16,13 +16,13 @@ Decoration containerDecoration({Color? color, BorderRadiusGeometry? borderRadius
 Decoration readDecoration() {
   return BoxDecoration(
     color: MyColors.read,
-    borderRadius: BorderRadius.circular(12),
+    borderRadius: .circular(12),
   );
 }
 // Padding containerPadding() {
 //   return EdgeInsets.symmetric(horizontal: 6, vertical: 2),
 // }
-EdgeInsetsGeometry unreadPadding = const EdgeInsets.symmetric(horizontal: 6, vertical: 2);
+EdgeInsetsGeometry unreadPadding = const .symmetric(horizontal: 6, vertical: 2);
 Decoration replyDecoration({Color? color, BorderRadiusGeometry? borderRadius, BoxBorder? border}) {
   return BoxDecoration(
     color: color,
