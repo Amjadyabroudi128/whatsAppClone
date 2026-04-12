2bff36fa16353df24bc7b0ac5dbe44387bbf79b3
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-04-12 18:35:56 +0100

    account screen

--- lib/features/Settings/Widget/accountFunctions/accountScreen.dart
+++ lib/features/Settings/Widget/accountFunctions/accountScreen.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/components/dividerWidget.dart';
 import 'package:whatsappclone/components/kCard.dart';
 import 'package:whatsappclone/features/Settings/Widget/accountFunctions/deleteAccount.dart';
 
 import '../../../../components/SizedBox.dart';
+import 'changeEmail.dart';
 
-class AccountScreen extends StatefulWidget {
-  const AccountScreen({super.key});
-
-  @override
-  State<AccountScreen> createState() => _AccountScreenState();
-}
-
-class _AccountScreenState extends State<AccountScreen> {
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({super.key, required this.passwordController, required this.email, required this.emailController});
+  final TextEditingController passwordController;
+  final String email;
+  final TextEditingController emailController;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +19,19 @@ class _AccountScreenState extends State<AccountScreen> {
           centerTitle: true,
           elevation: 0,
         ),
-      body:  const SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+      body:  SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
         child: Column(
           children: [
-            BoxSpacing(myHeight: 16),
+            const BoxSpacing(myHeight: 16),
             kCard(
               child: Column(
                 children: [
-                  deleteAccount(),
+                  const deleteAccount(),
+                  const divider(),
+                  ChangeEmail(passwordController: passwordController,
+                      email: email,
+                      emailController: emailController)
                 ],
               ),
             )
